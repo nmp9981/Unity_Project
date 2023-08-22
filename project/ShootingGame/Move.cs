9d6b5245c7b3e0a0f2a8917533f5d71913fd7bef
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-08-22 23:17:46 +0900

    Update Move.cs

--- project/ShootingGame/Move.cs
+++ project/ShootingGame/Move.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     [SerializeField] Animator moveAnim;
+
+    bool isleftColid, isrightColid, isupColid, isdownColid;//각 방향 충돌여부
     // Start is called before the first frame update
     void Awake()
     {
@@ -20,8 +22,9 @@ public class Move : MonoBehaviour
     private void MovePlayer()
     {
         float h = Input.GetAxisRaw("Horizontal");//좌우 즉시 이동
-        
+        if ((h == 1 && isrightColid) || (h == -1 && isleftColid)) h = 0;//이동 범위 초과
         float v= Input.GetAxisRaw("Vertical");//상하 즉시 이동
+        if ((v == 1 && isupColid) || (v == -1 && isdownColid)) v = 0;//이동 범위 초과
 
         Vector2 CurPos = transform.position;
         Vector2 deltaPos = new Vector2(h, v) * GamaManager.Instance.PlayerSpeed * Time.deltaTime;
@@ -30,6 +33,52 @@ public class Move : MonoBehaviour
         if(Input.GetButtonUp("Horizontal") || Input.GetButtonDown("Horizontal"))
         {
             moveAnim.SetInteger("Move", (int)h);
+        }
+    }
+    //충돌 영역에 들어옴
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //이동 가능 범위 초과
+        if(collision.tag == "Wall")
+        {
+            switch (collision.name)
+            {
+                case "RightWall":
+                    isrightColid = true;
+                    break;
+                case "LeftWall":
+                    isleftColid = true;
+                    break;
+                case "TopWall":
+                    isupColid = true;
+                    break;
+                case "BottomWall":
+                    isdownColid = true;
+                    break;
+            }
+        }
+    }
+
+    //충돌 영역을 나감
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Wall")
+        {
+            switch (collision.name)
+            {
+                case "RightWall":
+                    isrightColid = false;
+                    break;
+                case "LeftWall":
+                    isleftColid = false;
+                    break;
+                case "TopWall":
+                    isupColid = false;
+                    break;
+                case "BottomWall":
+                    isdownColid = false;
+                    break;
+            }
         }
     }
 }
