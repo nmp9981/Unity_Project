3a9a51a78cbf08961a79df79f575ab863b84d1db
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-09-04 21:32:52 +0900

    Update Bullet.cs

--- project/ShootingGame/Bullet.cs
+++ project/ShootingGame/Bullet.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public string ownerName;
+    public int hitAmount;
     float rotZ = 10.0f;//회전 각도
 
+    //피격 데미지 설정
+    private void Awake()
+    {
+        switch (ownerName)
+        {
+            case "EnemyA":
+                hitAmount = 2;
+                break;
+            case "EnemyC":
+                hitAmount = 3;
+                break;
+        }
+    }
     /*
     //총알 상시 회전
     private void Update()
@@ -17,6 +32,12 @@ public class Bullet : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Wall")
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        //적 총알이 플레이어에게 맞으면
+        if(collision.tag == "Player" && ownerName.Contains("Enemy"))
         {
             this.gameObject.SetActive(false);
         }
