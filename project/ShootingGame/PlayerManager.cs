7b96a82f734e85d5d75161f72a010b8ca95070bc
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-09-05 23:39:18 +0900

    Update PlayerManager.cs

--- project/ShootingGame/PlayerManager.cs
+++ project/ShootingGame/PlayerManager.cs
@@ -7,7 +7,9 @@ public class PlayerManager : MonoBehaviour
 {
     GamaManager gamaManager;
     ItemManager itemManager;
-    
+    UIManager uiManager;
+
+    public GameObject HPBarObject; 
     [SerializeField] GameObject HpSlider;
     [SerializeField] GameObject HPBar;
     [SerializeField] Text HPInfo;
@@ -16,14 +18,21 @@ public class PlayerManager : MonoBehaviour
     void Awake()
     {
         itemManager = GameObject.Find("ItemManager").GetComponent<ItemManager>();
+        uiManager = GameObject.Find("UIManager").GetComponent<UIManager>();
+        Instantiate(HPBarObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+        HPBarSetting();
+    }
+    //체력바 세팅
+    public void HPBarSetting()
+    {
         HpSlider.transform.position = Camera.main.WorldToScreenPoint(this.gameObject.transform.position + new Vector3(0, 1.0f, 0));
         HPInfo.transform.position = Camera.main.WorldToScreenPoint(this.gameObject.transform.position + new Vector3(0, 1.0f, 0));
-        HPInfo.text = GamaManager.Instance.HP.ToString()+" / " + GamaManager.Instance.FullHP.ToString();
+        HPInfo.text = GamaManager.Instance.HP.ToString() + " / " + GamaManager.Instance.FullHP.ToString();
         HPBar.GetComponent<Image>().fillAmount = (float)GamaManager.Instance.HP / (float)GamaManager.Instance.FullHP;
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -32,8 +41,7 @@ public class PlayerManager : MonoBehaviour
         if(collision.tag == "EnemyBullet" || collision.tag == "Enemy")
         {
             if (collision.tag == "Enemy") GamaManager.Instance.HP -= 1;//적 몸체에 맞으면 추가 데미지
-
-            GamaManager.Instance.HP -= 1;
+            else if(collision.tag == "EnemyBullet") GamaManager.Instance.HP -= collision.gameObject.GetComponent<Bullet>().hitAmount;//피격 데미지
             this.gameObject.SetActive(false);//피격하면 잠시 무적
             Invoke("ChangeImage", 0.3f);
         }
@@ -45,11 +53,16 @@ public class PlayerManager : MonoBehaviour
         }
         if(GamaManager.Instance.HP <= 0)
         {
-            GamaManager.Instance.GameOver();
+            uiManager.GameOver();
         }
     }
     void ChangeImage()
     {
         this.gameObject.SetActive(true);//원래대로
+    }
+    //HP바 파괴
+    public void HPBarDestroy()
+    {
+        Destroy(HPBarObject);
     }
 }
