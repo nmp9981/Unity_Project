5e5f2e048d46c171ed152596e74d3ed330ac85d9
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-09-06 21:39:55 +0900

    Update ReSpawnManager.cs

--- project/ShootingGame/ReSpawnManager.cs
+++ project/ShootingGame/ReSpawnManager.cs
@@ -6,14 +6,15 @@ public class ResPawnManager : MonoBehaviour
 {
     public ObjectManager objectManager;
     public string[] enemySpawn;//적 소환
-    Transform[] enemySpawnPos;//적 소환 위치
+    [SerializeField] Transform[] enemySpawnPos;//적 소환 위치
 
     float coolTime;
     float curTime;
+    float minCoolTime = 1.0f;
     // Start is called before the first frame update
     void Awake()
     {
-        enemySpawn = new string[] {"EnemyA"};//적 목록
+        enemySpawn = new string[] {"EnemyA", "EnemyB","EnemyC","EnemyD"};//적 목록
         curTime = 0.0f;
         coolTime = 2.0f;
     }
@@ -26,15 +27,20 @@ public class ResPawnManager : MonoBehaviour
         {
             SpawnEnemy();//적 소환
             curTime = 0.0f;
-            coolTime = Random.Range(1.8f, 3.8f);
+
+            coolTime = Random.Range(minCoolTime,Mathf.Max(minCoolTime+0.3f, 4.8f-(float)GamaManager.Instance.Stage));
         }
     }
     //적 소환
     void SpawnEnemy()
     {
-        int enemyIndex = 0;
+        int minIndex = GamaManager.Instance.Stage>4?1:0;
+        int maxIndex = Mathf.Min(4, GamaManager.Instance.Stage);
+        int enemyIndex = Random.Range(minIndex, maxIndex);//생성 범위
         //생성
+        int instiatePos = Random.Range(0, 6);//생성 위치
+
         GameObject enemy = objectManager.MakeGameObject(enemySpawn[enemyIndex]);
-        enemy.transform.position = new Vector3(Random.Range(-2,2), 4, 0);
+        enemy.transform.position = enemySpawnPos[instiatePos].position;
     }
 }
