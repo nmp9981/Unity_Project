68219b6d1b2bf08a26bf45e02f1bfcc4b7de01f0
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-09-06 21:39:23 +0900

    Update UIManager.cs

--- project/ShootingGame/UIManager.cs
+++ project/ShootingGame/UIManager.cs
@@ -10,6 +10,8 @@ public class UIManager : MonoBehaviour
     PlayerManager playerManager;
     [SerializeField] GameObject _gameOver;//게임 오버
     public TextMeshProUGUI expPoint;
+    public TextMeshProUGUI stagePoint;
+    public TextMeshProUGUI restMobPoint;
 
     private void Awake()
     {
@@ -19,10 +21,15 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         expPoint.text = "<size=100%>Score : </size=90%>" + GamaManager.Instance.Score.ToString();
+        stagePoint.text = "<size=100%>Stage : </size=80%>" + GamaManager.Instance.Stage.ToString();
+        restMobPoint.text = GamaManager.Instance.HitMonster.ToString()+" / "+ GamaManager.Instance.GoalMonster.ToString();
     }
     public void Init()
     {
         GamaManager.Instance.Score = 0;
+        GamaManager.Instance.Stage = 1;
+        GamaManager.Instance.HitMonster = 0;
+        GamaManager.Instance.GoalMonster = 5;
     }
     //득점
     public void getScore(string type)
@@ -38,8 +45,19 @@ public class UIManager : MonoBehaviour
             case "EnemyC":
                 GamaManager.Instance.Score += 6;
                 break;
+            case "EnemyD":
+                GamaManager.Instance.Score += 9;
+                break;
         }
-        GamaManager.Instance.Stage = 1+GamaManager.Instance.Score / 25;//스테이지 지정
+        GamaManager.Instance.HitMonster += 1;//몬스터를 잡음
+        if (GamaManager.Instance.HitMonster >= GamaManager.Instance.GoalMonster) NextStage();//스테이지 증가
+    }
+    //스테이지 증가
+    public void NextStage()
+    {
+        GamaManager.Instance.Stage += 1;
+        GamaManager.Instance.HitMonster = 0;
+        GamaManager.Instance.GoalMonster = 5+GamaManager.Instance.Stage/2;
     }
     //게임 오버
     public void GameOver()
@@ -55,6 +73,9 @@ public class UIManager : MonoBehaviour
         _gameOver.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         GamaManager.Instance.Score = 0;//점수 초기화
+        GamaManager.Instance.Stage = 1;//스테이지 초기화
+        GamaManager.Instance.HitMonster = 0;
+        GamaManager.Instance.GoalMonster = 5;
         GamaManager.Instance.HP = GamaManager.Instance.FullHP;//다시 풀피
     }
 }
