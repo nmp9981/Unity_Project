a171c08c4a116e2c328f4a8fafb798f02d287a37
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-10-04 18:05:35 +0900

    Update Item.cs

--- project/Quad_Action/Item.cs
+++ project/Quad_Action/Item.cs
@@ -8,9 +8,28 @@ public class Item : MonoBehaviour
     public Type type;//타입
     public int value;//값
 
+    Rigidbody rigid;
+    SphereCollider sphereCollider;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        //GetComponent는 첫번째 컴포넌트만 가져옴
+        sphereCollider = GetComponent<SphereCollider>();
+    }
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up * 25*Time.deltaTime);//10도씩 회전
+    }
+
+    //아이템이 바닥에 닿으면 콜라이더 해제
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            rigid.isKinematic = true;//물리 해제
+            sphereCollider.enabled = false;//콜라이더 비할성화
+        }
     }
 }
