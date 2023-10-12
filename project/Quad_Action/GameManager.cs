377a87c9c479dae78763b9197e1830689e7928fe
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-10-12 23:05:09 +0900

    Update GameManager.cs

--- project/Quad_Action/GameManager.cs
+++ project/Quad_Action/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class GameManager : MonoBehaviour
     public GameObject gameCamera;
     public Player player;
     public Boss boss;
+    public GameObject itemShop;
+    public GameObject weaponShop;
+    public GameObject startZone;
     
     public int stage;
     public float playTime;
@@ -17,9 +21,15 @@ public class GameManager : MonoBehaviour
     public int enemyCntA;//남은 적의 수
     public int enemyCntB;
     public int enemyCntC;
+    public int enemyCntD;
+
+    public Transform[] enemyZones;//적 스폰 위치
+    public GameObject[] enemies;//적들
+    public List<int> enemyList;
 
     public GameObject menuPanel;
     public GameObject gamePanel;
+    public GameObject overPanel;
     
     public Text maxScoreTxt;
     public Text scoreTxt;
@@ -42,11 +52,20 @@ public class GameManager : MonoBehaviour
 
     public RectTransform bossHealthGroup;
     public RectTransform bossHealthBar;
+
+    public Text curScoreText;
+    public Text bestScoreText;
     // Start is called before the first frame update
     void Awake()
     {
+        enemyList = new List<int>();
         maxScoreTxt.text = string.Format("{0:n0}", PlayerPrefs.GetInt("MaxScore"));//3자리 쉼표 주기
 
+        //첫 시작시 최고점 세팅
+        if (PlayerPrefs.HasKey("MaxScore"))
+        {
+            PlayerPrefs.SetInt("MaxScore", 0);
+        }
     }
 
     //메뉴관련 비활성화 게임관련 활성화
@@ -59,6 +78,107 @@ public class GameManager : MonoBehaviour
         gamePanel.SetActive(true);
 
         player.gameObject.SetActive(true);
+    }
+    //스테이지 관리
+    public void StageStart()
+    {
+        //전투가 시작되면 상점과 시작 존은 비활성화
+        itemShop.SetActive(false);
+        weaponShop.SetActive(false);
+        startZone.SetActive(false);
+
+        foreach (Transform zone in enemyZones) zone.gameObject.SetActive(true);//스폰 존은 활성화
+
+        isBattle = true;
+        StartCoroutine(InBattle());
+    } 
+    public void StageEnd()
+    {
+        isBattle = false;
+        player.transform.position = Vector3.up*1f;//플레이어 원위치
+
+        //전투가 끝났으므로 다시 활성화
+        itemShop.SetActive(true);
+        weaponShop.SetActive(true);
+        startZone.SetActive(true);
+        stage++;
+
+        foreach (Transform zone in enemyZones) zone.gameObject.SetActive(false);//스폰 존은 비활성화
+    }
+    //게임 오버
+    public void GameOver()
+    {
+        gamePanel.SetActive(false);
+        overPanel.SetActive(true);
+        curScoreText.text = scoreTxt.text;//현재 점수
+
+        //최고점 갱신
+        int maxScore = PlayerPrefs.GetInt("MaxScore");
+        if(player.score > maxScore)
+        {
+            bestScoreText.gameObject.SetActive(true);
+            PlayerPrefs.SetInt("MaxScore", player.score);
+        }
+    }
+    //메인으로
+    public void ReStart()
+    {
+        SceneManager.LoadScene(0);
+    }
+    IEnumerator InBattle()
+    {
+        //보스 
+        if (stage % 5 == 0)
+        {
+            enemyCntD++;
+            GameObject instantEnemy = Instantiate(enemies[3], enemyZones[0].position, enemyZones[0].rotation);
+            Enemy enemy = instantEnemy.GetComponent<Enemy>();
+            enemy.target = player.transform;//타겟은 플레이어
+            enemy.manager = this;//자기 자신, manager변수 채워주기
+            boss = instantEnemy.GetComponent<Boss>();//보스 변수 채워주기
+        }
+        else//일반 몹
+        {
+            //몬스터 정보 저장
+            for (int index = 0; index < stage; index++)
+            {
+                int ran = Random.Range(0, 3);
+                enemyList.Add(ran);
+
+                switch (ran)
+                {
+                    case 0:
+                        enemyCntA++;
+                        break;
+                    case 1:
+                        enemyCntB++;
+                        break;
+                    case 2:
+                        enemyCntC++;
+                        break;
+                }
+            }
+            //몬스터 소환
+            while (enemyList.Count > 0)
+            {
+                int ranZone = Random.Range(0, 4);
+                GameObject instantEnemy = Instantiate(enemies[enemyList[0]], enemyZones[ranZone].position, enemyZones[ranZone].rotation);
+                Enemy enemy = instantEnemy.GetComponent<Enemy>();
+                enemy.target = player.transform;//타겟은 플레이어
+                enemy.manager = this;//자기 자신, manager변수 채워주기
+                enemyList.RemoveAt(0);//맨 앞 삭제
+
+                yield return new WaitForSeconds(4f);//4초에 한번씩
+            }
+        }
+        //남은 몬스터 숫자 검사
+        while (enemyCntA + enemyCntB + enemyCntC + enemyCntD > 0)
+        {
+            yield return null;
+        }
+        yield return new WaitForSeconds(4f);
+        boss = null;//다시 원래대로
+        StageEnd();
     }
     //플레이 타임
     private void Update()
@@ -97,7 +217,15 @@ public class GameManager : MonoBehaviour
         enemyBTxt.text = enemyCntB.ToString();
         enemyCTxt.text = enemyCntC.ToString();
 
-        //보스 체력
-        bossHealthBar.localScale = new Vector3(Mathf.Max(0f,(float)boss.curHealth / (float)boss.maxHealth), 1, 1);
+        //보스 체력(보스가 있을때만)
+        if (boss != null)
+        {
+            bossHealthGroup.anchoredPosition = Vector3.down * 30;
+            bossHealthBar.localScale = new Vector3(Mathf.Max(0f, (float)boss.curHealth / (float)boss.maxHealth), 1, 1);
+        }
+        else
+        {
+            bossHealthGroup.anchoredPosition = Vector3.up * 200;
+        }
     }
 }
