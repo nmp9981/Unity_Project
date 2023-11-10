9993f8ae07db058428a5a5b19446eb487906cd57
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-11-10 16:59:09 +0900

    Create CubeStat.cs

---
+++ Study/DOTS/JobSystem/BallFlutter/CubeStat.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeStat : MonoBehaviour
+{
+    public Vector3 P0;
+    public Vector3 P1;
+    public Vector3 P2;
+    public Vector3 P3;
+}
