7fdf2724505a31e6d61b4bfd421c4a2e488a9384
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-11-22 14:00:14 +0900

    Update and rename Part11_CustomLightBasic.cs to Part11_LambertLight.cs

--- Study/Shader/Part11_LambertLight.cs
+++ Study/Shader/Part11_LambertLight.cs
@@ -1,9 +1,10 @@
-Shader "Custom/CustomLight"
+Shader"Custom/CustomLight"
 {
     Properties
     {
         
         _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _BumpMap("NormalMap", 2D) = "bump"{}
         
     }
     SubShader
@@ -17,23 +18,25 @@ Shader "Custom/CustomLight"
         #pragma target 3.0
 
         sampler2D _MainTex;
+        sampler2D _BumpMap;
 
         struct Input
         {
             float2 uv_MainTex;
+            float2 uv_BumpMap;
         };
-
-        
 
         void surf (Input IN, inout SurfaceOutput o)
         {
             fixed4 c = tex2D (_MainTex, IN.uv_MainTex);
             o.Albedo = c.rgb;
+            o.Normal = UnpackNormal(tex2D(_BumpMap, IN.uv_BumpMap));//노멀맵 추가
             o.Alpha = c.a;
         }
 float4 LightingTest(SurfaceOutput s, float3 lightDir, float atten)
 {
-    return float4(1, 0, 0, 1);
+    float ndotl = saturate(dot(s.Normal, lightDir));//0~1사이
+    return ndotl+0.5;
 }
         ENDCG
     }
