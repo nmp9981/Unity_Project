148ca49aaa73e0c741e97ad4e4b265589edb8004
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-12-09 23:31:10 +0900

    Create Part17_Alphablending_CustomControl.cs

---
+++ Study/Shader/Part17_Alphablending_CustomControl.cs
@@ -0,0 +1,37 @@
+Shader "Custom/CustomBlending"
+{
+    Properties
+    {
+        
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Transparent" "Queue" = "Transparent" }
+        zwrite off//z버퍼 끄기
+        blend SrcAlpha OneMinusSrcAlpha//블렌딩 옵션
+        LOD 200
+
+        CGPROGRAM
+        #pragma surface surf Lambert keepalpha
+
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        void surf (Input IN, inout SurfaceOutput o)
+        {
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex);
+            o.Albedo = c.rgb;
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Legacy Shaders/Transparent/VertexLit"
+}
