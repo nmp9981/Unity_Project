aad5b6859f5119937416cfd8ac60277fcdac485e
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-01-08 22:19:29 +0900

    Update DoubleDataStructure_II.cs

--- Study/DOTS/JobSystem/Example/DoubleDataStructure_II.cs
+++ Study/DOTS/JobSystem/Example/DoubleDataStructure_II.cs
@@ -5,6 +5,7 @@ using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities.UniversalDelegates;
 using Unity.Jobs;
+using Unity.Logging.Sinks;
 using UnityEngine;
 
 public class NativeDoubleSystem : MonoBehaviour
@@ -31,35 +32,47 @@ public class NativeDoubleSystem : MonoBehaviour
        
         public void Execute()
         {
-            
             for (int i = 0; i < 3; i++)
             {
-                hashSet = new UnsafeHashSet<int>(11,Allocator.TempJob);
-                for (int j = 0; j < 3; j++)
+                hashSet = new UnsafeHashSet<int>(11,Allocator.TempJob);//할당
+                for (int j = 1; j <= 4; j++)
                 {
-                    hashSet.Add(a * i + j);//3*i+j
+                    hashSet.Add(a * i + j);//4*i+j
                 }
                 duoSet[i] = hashSet;
-                foreach (int x in duoSet[i])
-                {
-                    Debug.Log(x + "\n");
-                }
-                hashSet.Dispose();
+                hashSet.Dispose();//파괴
             }
-            for(int i = 0; i < 3; i++)
+        }
+    }
+    struct JobSingle3 : IJob
+    {
+        [NativeDisableContainerSafetyRestriction]
+        public NativeArray<UnsafeQueue<int>> duoQueue;//하나씩 분리
+
+        public UnsafeQueue<int> jobQueue;//큐 자료구조
+
+        public void Execute()
+        {
+            for (int i = 0; i < 3; i++)
             {
-                foreach (int x in duoSet[i])
+                jobQueue = new UnsafeQueue<int>(Allocator.TempJob);//할당
+                for (int j = 7; j <= 9; j++)
                 {
-                    Debug.Log(x + "\n");
+                    jobQueue.Enqueue((i+7) * j);//4*i+j
                 }
+                duoQueue[i] = jobQueue;
+                //jobQueue.Dispose();//파괴
             }
         }
     }
     UnsafeHashSet<int> hashSet;
     public NativeArray<UnsafeHashSet<int>> douSet;
-   
+    public NativeArray<UnsafeQueue<int>> duoQueue;
+
+
     private void Start()
     {
+        //작업1에 대한 데이터 세팅
         NativeArray<int> array = new NativeArray<int>(11, Allocator.TempJob);
         JobSingle jobSingle = new JobSingle();
         jobSingle.a = 15;
@@ -83,10 +96,26 @@ public class NativeDoubleSystem : MonoBehaviour
         jobSingle2.duoSet = douSet;
         
         handle2 = jobSingle2.Schedule();
-        
+
+        //작업3에 대한 데이터 세팅
+        UnsafeQueue<int> jobQueue = new UnsafeQueue<int>(Allocator.TempJob);
+        duoQueue = new NativeArray<UnsafeQueue<int>>(11, Allocator.TempJob);
+
+        JobSingle3 jobSingle3;
+        JobHandle handle3;
+
+        jobSingle3 = new JobSingle3();
+
+        jobSingle3.jobQueue = jobQueue;
+        jobSingle3.duoQueue = duoQueue;
+
+        handle3 = jobSingle3.Schedule();
+
         // 메인스레드가 잡의 종료 대기
         handle.Complete();
         handle2.Complete();//여기서 Execute가 실행
+        handle3.Complete();
+
         
         //Job을 실행 할 수 있도록 워커 스레드에 예약함, 
         //메인 스레드에서는 Schedule만 호출 할 수 있음
@@ -102,10 +131,21 @@ public class NativeDoubleSystem : MonoBehaviour
             }
             
         }
-        
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j=0;j<3;j++)
+            {
+                int k = duoQueue[i].Peek();
+                duoQueue[i].Dequeue();
+                Debug.Log("Num " + i + " result3 : " + k + "\n");
+            }
+            
+        }
+
         // NativeContainer를 사용한 후에는 Dispose를 호출해서 메모리에서 삭제해야함.
         // Dispose를 호출하지 않으면 Unity에서 에러를 냄 
         jobSingle.array.Dispose();
         jobSingle2.duoSet.Dispose();
+        jobSingle3.duoQueue.Dispose();
     }
 }
