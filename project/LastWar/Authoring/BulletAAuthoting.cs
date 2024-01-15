969d6dfdde7ee8ce07191264a1479bef0951852d
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-01-16 03:44:37 +0900

    Create BulletAAuthoting.cs

---
+++ project/LastWar/Authoring/BulletAAuthoting.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace LastWar
+{
+    public class BulletAAuthoting : MonoBehaviour
+    {
+        public int Atk;
+        class Baker : Baker<BulletAAuthoting>
+        {
+            public override void Bake(BulletAAuthoting authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new BulletAObject
+                {
+                    Atk = 1
+                });
+                
+            }
+        }
+        public struct BulletAObject : IComponentData
+        {
+            public int Atk;
+        }
+    }
+}
