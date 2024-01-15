0f2d541786bab149def78b04bcf14281b65aaafa
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-01-16 03:44:15 +0900

    Update Config.cs

--- project/LastWar/Authoring/Config.cs
+++ project/LastWar/Authoring/Config.cs
@@ -1,39 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Entities;
 using UnityEngine;
+using Unity.Entities;
+using UnityEditor.PackageManager;
 
-namespace LastWar{
-    public class Config : MonoBehaviour
+namespace LastWar
+{
+    public class ConfigAuthoring : MonoBehaviour
     {
-        GameObject EnemyAObj;//적A
-        GameObject EnemyBObj;//적B
-        public float monsterOffset;//몬스터 간격
+        //캐릭터 오브젝트
+        public GameObject chracterPrefab;
+        //몬스터 오브젝트
+        public GameObject EnemyAPrefab;
+        public GameObject EnemyBPrefab;
+        
+        //총알 오브젝트
+        public GameObject BulletAPrefab;
+        public GameObject BulletBPrefab;
 
-        class Baker : Baker<Config>
+        //캐릭터 관련 변수
+        public float characterMoveSpeed;
+        //총알 관련 변수
+        public float bulletAMoveSpeed;
+        public float bulletBMoveSpeed;
+        //몬스터 관련 변수
+        public int spawnMonsterCount;
+
+        class Baker : Baker<ConfigAuthoring>
         {
-            public override void Bake(Config authoring)
+            public override void Bake(ConfigAuthoring authoring)
             {
-                var entity = GetEntity(TransformUsageFlags.None);//엔티티 화
-                AddComponent(entity, new ConfigData
+                var entity = GetEntity(TransformUsageFlags.Dynamic);//엔티티 화
+                AddComponent(entity, new Config
                 {
-                    monsterOffset = authoring.monsterOffset,
-                    EnemyAObj = GetEntity(authoring.EnemyAObj, TransformUsageFlags.Dynamic),
-                    EnemyBObj = GetEntity(authoring.EnemyBObj, TransformUsageFlags.Dynamic)
+                    EnemyAPrefab = GetEntity(authoring.EnemyAPrefab, TransformUsageFlags.Dynamic),
+                    EnemyBPrefab = GetEntity(authoring.EnemyBPrefab, TransformUsageFlags.Dynamic),
+                    chracterPrefab = GetEntity(authoring.chracterPrefab, TransformUsageFlags.Dynamic),
+                    BulletAPrefab = GetEntity(authoring.BulletAPrefab, TransformUsageFlags.Dynamic),
+                    BulletBPrefab = GetEntity(authoring.BulletBPrefab, TransformUsageFlags.Dynamic),
+                    characterMoveSpeed = authoring.characterMoveSpeed,
+                    bulletAMoveSpeed = authoring.bulletAMoveSpeed,
+                    bulletBMoveSpeed = authoring.bulletBMoveSpeed,
+                    spawnMonsterCount = 0
                 });
-
-                AddComponent<SpawnSystem>(entity);
+                AddComponent<EnemySpawner>(entity);
+                AddComponent<ChatacterSpawner>(entity);
+                AddComponent<CharacterMove>(entity);
+                AddComponent<CharacterAttack>(entity);
+                AddComponent<BulletSpawner>(entity);
             }
         }
-        
-        public struct ConfigData : IComponentData
+        public struct Config : IComponentData
         {
-            public Entity EnemyAObj;
-            public Entity EnemyBObj;
-            public float monsterOffset;
+            public Entity chracterPrefab;
+            public Entity EnemyAPrefab;
+            public Entity EnemyBPrefab;
+            public Entity BulletAPrefab;
+            public Entity BulletBPrefab;
+            
+            public float characterMoveSpeed;
+            public float bulletAMoveSpeed;
+            public float bulletBMoveSpeed;
+            public int spawnMonsterCount;
         }
 
-        public struct SpawnSystem : IComponentData
+        public struct EnemySpawner : IComponentData
+        {
+
+        }
+        public struct ChatacterSpawner : IComponentData
+        {
+
+        }
+        public struct CharacterMove : IComponentData
+        {
+
+        }
+        public struct CharacterAttack : IComponentData
+        {
+
+        }
+        public struct BulletSpawner : IComponentData
         {
 
         }
