67d980c8698a19534002797b8d9531ca62c3823d
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-01-17 04:23:48 +0900

    Create MoveEnemySystem.cs

---
+++ project/LastWar/System/MoveEnemySystem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using static LastWar.CharacterAuthoring;
+using static LastWar.ConfigAuthoring;
+using static LastWar.EnemyAAuthoring;
+
+namespace LastWar
+{
+    public partial struct MoveEnemySystem : ISystem    {
+
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<Config>();
+            state.RequireForUpdate<EnemyMove>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var config = SystemAPI.GetSingleton<Config>();
+            var moveAmount = new float3(0, 0, -0.3f) * SystemAPI.Time.DeltaTime;
+
+            foreach (var playerTransform in
+                     SystemAPI.Query<RefRW<LocalTransform>>()
+                         .WithAll<EnemyAObject>())
+            {
+                playerTransform.ValueRW.Position += moveAmount;//이동 후 위치
+            }
+        }
+    }
+
+}
