e125b8ccf55210a389400d60e690a099cccbeb6a
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-01-19 02:54:05 +0900

    Update EnemyBAuthoring.cs

--- project/LastWar/Authoring/EnemyBAuthoring.cs
+++ project/LastWar/Authoring/EnemyBAuthoring.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Entities;
 using UnityEngine;
+using Unity.Entities;
+using static LastWar.EnemyAAuthoring;
 
 namespace LastWar
 {
     public class EnemyBAuthoring : MonoBehaviour
     {
-        public int HP = 8;
-        public int Exp = 4;
+        public int HP;
         class Baker : Baker<EnemyBAuthoring>
         {
             public override void Bake(EnemyBAuthoring authoring)
@@ -17,17 +17,15 @@ namespace LastWar
                 var entity = GetEntity(TransformUsageFlags.Dynamic);
                 AddComponent(entity, new EnemyBObject
                 {
-                    HP = authoring.HP,
-                    Exp = authoring.Exp
-                }) ;
-                AddComponent<EnemyBObject>(entity);
+                    HP = authoring.HP
+                });
             }
         }
 
         public struct EnemyBObject : IComponentData
         {
             public int HP;
-            public int Exp;
         }
     }
+
 }
