d98b9956d1a9702c05c7fe46e3a290ea92b8bee6
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-02-05 00:34:09 +0900

    Update EnemyAObject.cs

--- project/LastWar/Authoring/EnemyAObject.cs
+++ project/LastWar/Authoring/EnemyAObject.cs
@@ -1,30 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Unity.Entities;
+using UnityEngine;
 
 namespace LastWar
 {
     public class EnemyAAuthoring : MonoBehaviour
     {
-        public int HP = 5;
-        class Baker: Baker<EnemyAAuthoring>
+        public int HP;
+        public int Exp;
+        public GameObject hpBar;
+        class Baker : Baker<EnemyAAuthoring>
         {
             public override void Bake(EnemyAAuthoring authoring)
             {
                 //데이터 초기화
                 var entity = GetEntity(TransformUsageFlags.Dynamic);
-                AddComponent(entity, new EnemAObject
+                AddComponent(entity, new EnemyAObject
                 {
-                    HP = authoring.HP
-                }) ;
-                AddComponent<EnemAObject>(entity);
+                    HP = authoring.HP,
+                    Exp = authoring.Exp,
+                    hpBar = GetEntity(authoring.hpBar, TransformUsageFlags.None)
+                });
             }
         }
-
-        public struct EnemAObject : IComponentData
+        
+        public struct EnemyAObject : IComponentData
         {
             public int HP;
+            public int Exp;
+            public Entity hpBar;
         }
     }
 
