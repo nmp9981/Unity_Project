6a0a66e71efb0a7ee7ee2ef09eb06278e77437c4
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-02-05 00:24:56 +0900

    Update UIManager.cs

--- project/LastWar/System/UIManager.cs
+++ project/LastWar/System/UIManager.cs
@@ -5,16 +5,13 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 using static LastWar.BulletAAuthoting;
+using static LastWar.CharacterAuthoring;
 using static LastWar.ConfigAuthoring;
 
 namespace LastWar
 {
     public class UIManager : MonoBehaviour
     {
-        [Header("Pivot")]
-        [SerializeField] private RectTransform m_Pivot;
-        [SerializeField] private float m_Offset;
-
         [Header("HP")]
         [SerializeField] private Slider playerHP;//플레이어 HP바
         [SerializeField] private TMPro.TextMeshProUGUI textPlayerHP;
@@ -24,50 +21,41 @@ namespace LastWar
         [SerializeField] private Slider m_SliderExpGauge;//경험치 바
         [SerializeField] private TMPro.TextMeshProUGUI m_TextExp;
 
-        public TMPro.TextMeshProUGUI hitDamageText;//피격 데미지
+        //public TMPro.TextMeshProUGUI hitDamageText;//피격 데미지
 
-        private Canvas m_Canvas;
-        private RectTransform m_Rectransform;
-
-
+     
         void Awake()
         {
-            m_Canvas = GetComponent<Canvas>();
-            m_Rectransform = GetComponent<RectTransform>();
            
-            if (playerHP != null) playerHP.value = 0f;
-            if (textPlayerHP != null) textPlayerHP.text = string.Empty;
-            if (m_TextLevel != null) m_TextLevel.text = string.Empty;
-            if (m_SliderExpGauge != null) m_SliderExpGauge.value = 0f;
-            if (m_TextExp != null) m_TextExp.text = string.Empty;
+            playerHP.value = 1f;
+            textPlayerHP.text = string.Empty;
+            m_TextLevel.text = string.Empty;
+            m_SliderExpGauge.value = 0f;
+            m_TextExp.text = string.Empty;
 
-            hitDamageText.text = string.Empty;
+            //hitDamageText.text = string.Empty;
         }
 
         //캐릭터의 정보 수정은 액션 마친 뒤 여기서 진행
-        void LateUpdate()
+        void Update()
         {
+            Debug.Log("씨발1");
             var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-            var query = entityManager.CreateEntityQuery(typeof(Config),typeof(ECSPlayerData));
-            if (!query.IsEmpty)//쿼리를 싱글톤으로
-            {
-                var entity = query.GetSingletonEntity();
-                var configData = entityManager.GetComponentData<Config>(entity);
-                var ecsPlayerData = entityManager.GetComponentData<ECSPlayerData>(entity);
+            var query = entityManager.CreateEntityQuery(typeof(ECSPlayerData));
+            Debug.Log("씨발2");
+            var entity = query.GetSingletonEntity();
+            var ecsPlayerData = entityManager.GetComponentData<ECSPlayerData>(entity);
 
-                if (m_Pivot != null)//피벗 위치 고정
-                {
-                    m_Pivot.anchoredPosition = Vector2.zero;
-                }
-
-                if (playerHP != null) playerHP.value = (float) ecsPlayerData.HP/ecsPlayerData.maxHP;
-                if (textPlayerHP != null) textPlayerHP.text = string.Format("{0}/{1}", ecsPlayerData.HP, ecsPlayerData.maxHP);
-                if (m_TextLevel != null) m_TextLevel.text = string.Format("Lv.{0}", ecsPlayerData.Lv);
-                if (m_SliderExpGauge != null) m_SliderExpGauge.value = (float)ecsPlayerData.Exp/ecsPlayerData.maxExp;
-                if (m_TextExp != null) m_TextExp.text = string.Format("{0}/{1}", ecsPlayerData.Exp, ecsPlayerData.maxExp);
-            }
+            Debug.Log(ecsPlayerData.Lv);
+            playerHP.value = (float)ecsPlayerData.HP / ecsPlayerData.maxHP;
+            textPlayerHP.text = string.Format("HP {0}/{1}", ecsPlayerData.HP, ecsPlayerData.maxHP);
+            m_TextLevel.text = string.Format("Lv.{0}", ecsPlayerData.Lv);
+            m_SliderExpGauge.value = (float)ecsPlayerData.Exp / ecsPlayerData.maxExp;
+            m_TextExp.text = string.Format("EXP {0}/{1}", ecsPlayerData.Exp, ecsPlayerData.maxExp);
+            
 
         }
+        /*
         public void DisplayHitDamage(int damage, float3 mobTransform)
         {
             hitDamageText.transform.position = mobTransform;
@@ -78,6 +66,7 @@ namespace LastWar
         {
             hitDamageText.text = string.Empty;
         }
+        */
     }
 
 }
