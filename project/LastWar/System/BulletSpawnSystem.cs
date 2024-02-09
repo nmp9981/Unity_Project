92a0edfbf2ff68c8e71bdc5ad4a68a3801d22cac
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-02-10 04:02:05 +0900

    Update BulletSpawnSystem.cs

--- project/LastWar/System/BulletSpawnSystem.cs
+++ project/LastWar/System/BulletSpawnSystem.cs
@@ -28,22 +28,40 @@ namespace LastWar
             //총알 생성  : 스페이스 바
             if (!Input.GetKeyDown(KeyCode.Space)) return;
 
-            //캐릭터의 위치에 소환
-            foreach (var (transform, entity) in SystemAPI.Query<RefRW<LocalTransform>>().WithAll<CharacterObject>().WithEntityAccess())
+            //총을 몇발 쏠지
+            int rowShotCount = 0;
+            int colShotCount = 0;
+            foreach (var playData in SystemAPI.Query<RefRW<ECSPlayerData>>())
             {
-                var bullet = state.EntityManager.Instantiate(config.BulletAPrefab);
-                state.EntityManager.SetComponentData(bullet, new LocalTransform
-                {
-                    Position = new float3
-                    {
-                        x = transform.ValueRO.Position.x,
-                        y=0.5f,
-                        z= transform.ValueRO.Position.z
-                    },
-                    Scale = 0.3f,
-                    Rotation = Quaternion.identity
-                });
+                playData.ValueRW.stepCount = math.min(6, playData.ValueRO.Lv-79);
+                playData.ValueRW.widthCount = 1;
+                rowShotCount = playData.ValueRO.widthCount;
+                colShotCount = playData.ValueRO.stepCount;
             }
+
+            //캐릭터의 위치에 소환
+            for (int i = 0; i < colShotCount; i++)
+            {
+                for(int j = 0; j < 1; j++)
+                {
+                    foreach (var (transform, entity) in SystemAPI.Query<RefRW<LocalTransform>>().WithAll<CharacterObject>().WithEntityAccess())
+                    {
+                        var bullet = state.EntityManager.Instantiate(config.BulletAPrefab);
+                        state.EntityManager.SetComponentData(bullet, new LocalTransform
+                        {
+                            Position = new float3
+                            {
+                                x = transform.ValueRO.Position.x + j,
+                                y = 0.5f,
+                                z = transform.ValueRO.Position.z+i*0.35f
+                            },
+                            Scale = 0.3f,
+                            Rotation = Quaternion.identity
+                        });
+                    }
+                }
+            }
+            
         }
     }
 
