05558cb1a27fb58969a52507821358374afc56e1
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-02-10 04:16:45 +0900

    Update EnemySpawnerSystem.cs

--- project/LastWar/System/EnemySpawnerSystem.cs
+++ project/LastWar/System/EnemySpawnerSystem.cs
@@ -21,20 +21,20 @@ public partial struct EnemySpawnerSystem : ISystem
     {
         state.RequireForUpdate<Config>();
         state.RequireForUpdate<EnemySpawner>();
-        coolTime = 3.0f;
-        nowTime = 0f;
+        coolTime = 8.0f;
+        nowTime = 7.0f;
     }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         var config = SystemAPI.GetSingleton<Config>();
-        if (config.spawnMonsterCount > 0) return;
+        //if (config.spawnMonsterCount > 0) return;
 
         var rand = new Unity.Mathematics.Random(100);//랜덤 시드
         foreach (var configEntity in SystemAPI.Query<RefRW<Config>>())
         {
-            if (configEntity.ValueRO.spawnMonsterCount == 0)
+            if (configEntity.ValueRO.spawnMonsterCount >= 0)
             {
                 nowTime+=Time.deltaTime;
                 if (nowTime < coolTime) continue;//쿨타임이 안옴
@@ -52,13 +52,13 @@ public partial struct EnemySpawnerSystem : ISystem
                             {
                                 Position = new float3
                                 {
-                                    x = i * 1.1f,
+                                    x = i * UnityEngine.Random.Range(0.8f, 2.0f),
                                     y = 0.5f,
-                                    z = i * 0.4f
+                                    z = UnityEngine.Random.Range(2.0f, 7.0f)
                                 },
                                 Scale = 1,
-                                Rotation = quaternion.identity
-                            });
+                                Rotation = quaternion.RotateX(-90)
+                            }) ;
                             break;
                         case 1:
                             var monster2 = state.EntityManager.Instantiate(config.EnemyBPrefab);//몬스터 생성
@@ -67,30 +67,16 @@ public partial struct EnemySpawnerSystem : ISystem
                             {
                                 Position = new float3
                                 {
-                                    x = i * 1.1f,
+                                    x = i * UnityEngine.Random.Range(1.0f, 2.0f),
                                     y = 0.5f,
-                                    z = i * 0.4f
+                                    z = UnityEngine.Random.Range(2.0f, 7.0f)
                                 },
                                 Scale = 1,
-                                Rotation = quaternion.identity
+                                Rotation = quaternion.RotateX(-90)
                             });
                             break;
                     }
-                    /*
-                    var monster = state.EntityManager.Instantiate(config.EnemyAPrefab);//몬스터 생성
-                                                                                       //몬스터 생성 위치 : 캐릭터의 위치
-                    state.EntityManager.SetComponentData(monster, new LocalTransform
-                    {
-                        Position = new float3
-                        {
-                            x = i*1.1f,
-                            y = 0.5f,
-                            z = i * 0.4f
-                        },
-                        Scale = 1,
-                        Rotation = quaternion.identity
-                    });
-                    */
+                    
                     configEntity.ValueRW.spawnMonsterCount += 1;
                 }
                 nowTime = 0;//시간 초기화
