d4cd778b9ab44880a4da6db372b6d2794335879e
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-02-10 18:36:23 +0900

    Create CastleHPSystem.cs

---
+++ project/LastWar/System/CastleHPSystem.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+using static LastWar.CharacterAuthoring;
+using static LastWar.ConfigAuthoring;
+using static LastWar.EnemyAAuthoring;
+using static LastWar.EnemyBAuthoring;
+using static UnityEngine.EventSystems.EventTrigger;
+
+namespace LastWar
+{
+    [UpdateBefore(typeof(TransformSystemGroup))]
+    public partial struct CastleHPSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<Config>();
+            state.RequireForUpdate<CastleObject>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var config = SystemAPI.GetSingleton<Config>();
+            var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+            var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+
+            float minDistSq = 5;
+            foreach (var castleTransform in
+                    SystemAPI.Query<RefRW<LocalTransform>>()
+                        .WithAll<CastleObject>())//성벽 위치
+            {
+                
+                //적들 정보
+                foreach (var (enemyTransform, enemyInfo, EnemyEntity) in SystemAPI.Query<RefRW<LocalTransform>, RefRW<EnemyAObject>>().WithAll<EnemyAObject>().WithEntityAccess())
+                {
+                    Debug.Log(enemyTransform.ValueRO.Position.z + "@@@@성 좌표" + math.distancesq(enemyTransform.ValueRO.Position.z, castleTransform.ValueRO.Position.z));
+                    //적에 닿으면 파괴
+                    if (math.distancesq(enemyTransform.ValueRO.Position.z, castleTransform.ValueRO.Position.z) <= minDistSq)
+                    {
+                        //피격 데미지
+                        foreach (var playData in SystemAPI.Query<RefRW<ECSPlayerData>>())
+                        {
+                            playData.ValueRW.HP -= 5;
+                            //사망 처리
+                            if(playData.ValueRO.HP <= 0)
+                            {
+
+                            }
+                            break;
+                        }
+                        ecb.DestroyEntity(EnemyEntity);//적 파괴
+                        break;
+                    }
+                }
+                
+                foreach (var (enemyTransform, enemyInfo, EnemyEntity) in SystemAPI.Query<RefRW<LocalTransform>, RefRW<EnemyBObject>>().WithAll<EnemyBObject>().WithEntityAccess())
+                {
+                    //적에 닿으면 파괴
+                    if (math.distancesq(enemyTransform.ValueRO.Position, castleTransform.ValueRO.Position) <= minDistSq)
+                    {
+                        //피격 데미지
+                        foreach (var playData in SystemAPI.Query<RefRW<ECSPlayerData>>())
+                        {
+                            playData.ValueRW.HP -= 3;
+                            //사망 처리
+                            if (playData.ValueRO.HP <= 0)
+                            {
+
+                            }
+                            break;
+                        }
+                        ecb.DestroyEntity(EnemyEntity);//적 파괴
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
