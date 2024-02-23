f63389671adcc1eff9e821fd4d4b0e791161030c
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-02-24 03:51:04 +0900

    Update FishesSpawn.cs

--- project/FishDom/Manager/FishesSpawn.cs
+++ project/FishDom/Manager/FishesSpawn.cs
@@ -17,16 +17,23 @@ public class FishesSpawn : MonoBehaviour
     {
         while (true)
         {
-            int mobCount = UnityEngine.Random.Range(1, 5);
+            int mobCount = UnityEngine.Random.Range(5, 9);
             GameObject[] fishobjects = _objectFull.GetPool(GameManager.Instance.StageNum - 1);
             for (int i = 0; i < mobCount; i++)
             {
-                int spawnX = UnityEngine.Random.Range(-5, 5);
-                int spawnY = UnityEngine.Random.Range(-5, 5);
+                int spawnX = UnityEngine.Random.Range(2, 6);
+                int spawnY = UnityEngine.Random.Range(2, 6);
+                int spawnMinus = (UnityEngine.Random.Range(-5, 5)>=0)?-1:1;
                 GameObject gm = _objectFull.MakeObj(GameManager.Instance.StageNum - 1);
-                gm.transform.position = new Vector3(target.transform.position.x + spawnX, target.transform.position.y + spawnY, 0);
+                gm.transform.position = new Vector3(target.transform.position.x + spawnX*spawnMinus, target.transform.position.y + spawnY*spawnMinus, 0);
             }
             yield return new WaitForSeconds(5.0f);
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
     }
 }
