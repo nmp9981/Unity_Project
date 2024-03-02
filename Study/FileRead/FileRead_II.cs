a18d915e9184b4b5c40e3b985f61ab4f99989fe6
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-03-02 17:46:53 +0900

    Update FileRead_II.cs

--- Study/FileRead/FileRead_II.cs
+++ Study/FileRead/FileRead_II.cs
@@ -1,42 +1,63 @@
+using Dummiesman;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.IO;
-using UnityEngine.Pixyz;
-using UnityEngine.Pixyz.API;
-using Assimp;
-using UnityEditor.PixyzPlugin4Unity.Toolbox;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class RVMImporter : MonoBehaviour
+public class Importer : MonoBehaviour
 {
-    string path = "C:\\Users\\young\\OneDrive\\바탕 화면\\RMVATT-BACKUP-240222\\Outf20240227";
-    //DirectoryInfo로 폴더정보 불러오기
-    System.IO.DirectoryInfo di = new DirectoryInfo("C:\\Users\\young\\OneDrive\\바탕 화면\\RMVATT-BACKUP-240222\\Outf20240227\\TwoBlockCopy");
-    void Awake()
+    string path = "C:/ObjectSet/ObjectSetCopy";
+    string SuccessPath = "C:/ObjectSet/ObjectSetCopy" + "/Success";
+    string FailPass = "C:/ObjectSet/ObjectSetCopy" + "/Fail";
+
+    System.IO.DirectoryInfo di = new DirectoryInfo("C:/ObjectSet/ObjectSetCopy");
+    private void Awake()
     {
         FileCreate();
         AutoImport();
     }
-
     void FileCreate()
     {
-        if(!File.Exists(path + "/Success")) Directory.CreateDirectory(path + "/Success");
-        if(!File.Exists(path + "/Fail")) Directory.CreateDirectory(path + "/Fail");
+        if (!File.Exists(path + "/Success")) Directory.CreateDirectory(path + "/Success");
+        if (!File.Exists(path + "/Fail")) Directory.CreateDirectory(path + "/Fail");
     }
     void AutoImport()
     {
-        string pullPath = "C:\\Users\\young\\OneDrive\\바탕 화면\\RMVATT-BACKUP-240222\\Outf20240227\\TwoBlockCopy";
-
         int cnt = 0;
         //해당 디렉토리를 순회하면서 파일이름 불러오기
         foreach (FileInfo file in di.GetFiles("*.rvm"))//rvm로 끝나는 파일만 검색
         {
             Debug.Log(file.FullName);
-            
-            cnt++;
-            if(cnt>10) break;
-        }
-       
-    }
+            Debug.Log(file.Name);
 
+            bool prefabSuccess;
+            string localPath = "Assets/Prefabs/" + gameObject.name + ".prefab";
+            localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
+            GameObject pPrefab = PrefabUtility.SaveAsPrefabAsset(file, localPath, out prefabSuccess);
+            Instantiate(pPrefab);
+            
+            //GameObject gm = new OBJLoader().Load(file.FullName);
+            //Instantiate(gm);
+            
+            if (GameObject.Find(pPrefab.name))//성공
+            {
+                Debug.Log("성공");
+                System.IO.File.Copy(file.FullName, SuccessPath+"/"+file.Name, true);//해당 경로로 파일 옮기기, true로 하면 덮어쓰기가 된다.
+            }
+            else//실패
+            {
+                Debug.Log("실패");
+                System.IO.File.Copy(file.FullName, FailPass+"/"+file.Name, true);
+            }
+            
+            AssetDatabase.Refresh();
+            EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());//현재 씬 자동 저장
+            cnt++;
+            if (cnt > 5) break;
+        }
+
+    }
 }
