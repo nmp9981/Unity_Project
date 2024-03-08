d6a4c9ec4c795cae2a489d8e87b9b068bb7749ef
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-03-09 00:16:33 +0900

    Update BoundObject.cs

--- Study/Bound/BoundObject.cs
+++ Study/Bound/BoundObject.cs
@@ -1,13 +1,18 @@
-Scene sc = SceneManager.GetActiveScene();
-
-Bounds bounds = default;
-foreach (GameObject go in sc.GetRootGameObjects())
-{
-    if (go.GetComponent<RTComponent>() is var component && component != null)
+    public void SelectioView()
     {
-        if (bounds == default) bounds = component.GetBounds(false);
-        else bounds.Encapsulate(go.GetComponent<BoxCollider>().bounds);
-    }
+        Camera.main.gameObject.transform.localRotation = Quaternion.Euler(90, 0, 0);
+        Scene sc = SceneManager.GetActiveScene();
 
+        Bounds bounds = default;
+        foreach (GameObject go in sc.GetRootGameObjects())
+        {
+            if (go.GetComponent<RTComponent>() is var component && component != null)
+            {
+                if (bounds == default) bounds = component.GetBounds(false);
+                else bounds.Encapsulate(go.GetComponent<Bounds>());
+            }
+
+        }
+        Camera.main.gameObject.transform.position = new Vector3(bounds.center.x, bounds.center.y+80, bounds.center.z);
+    }
 }
-Camera.main.gameObject.transform.position = new Vector3(bounds.center.x, bounds.center.y, bounds.center.z+50);
