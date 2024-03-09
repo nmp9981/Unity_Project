8a5d32fdea98af7079dd33a5ef4cdef21673cb7d
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-03-10 02:43:59 +0900

    Update InGameUI.cs

--- project/FishDom/Manager/InGameUI.cs
+++ project/FishDom/Manager/InGameUI.cs
@@ -7,12 +7,19 @@ public class InGameUI : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _restCountFishText;
     [SerializeField] GameObject _settingBoard;
+    [SerializeField] GameObject _returnButton;
     void Start()
     {
-        if (GameManager.Instance.PlayMode != 0)
+        if (GameManager.Instance.PlayMode != 0)//스테이지 모드
         {
             GameManager.Instance.RestCount = GameManager.Instance.StageNum * 7;
-        } 
+            _returnButton.SetActive(false);
+        }
+        else//챌린지 모드
+        {
+            _returnButton.SetActive(true);
+        }
+        
     }
 
     // Update is called once per frame
