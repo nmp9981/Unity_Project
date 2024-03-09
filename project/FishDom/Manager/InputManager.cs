6805a3c59d29a6e782bb65e80aa39deaf097de58
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-03-10 02:33:20 +0900

    Update InputManager.cs

--- project/FishDom/Manager/InputManager.cs
+++ project/FishDom/Manager/InputManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -103,6 +104,7 @@ public class InputManager : MonoBehaviour
     }
     public void GoMainScene()
     {
+        GameManager.Instance.PlayerHit = true;
         SceneManager.LoadScene("MainMenu");
     }
 }
