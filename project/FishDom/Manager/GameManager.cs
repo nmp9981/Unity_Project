5d99af639748a987186dda69d5207ae5e4bda7d2
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-03-15 02:20:29 +0900

    Update GameManager.cs

--- project/FishDom/Manager/GameManager.cs
+++ project/FishDom/Manager/GameManager.cs
@@ -8,7 +8,6 @@ public class GameManager : MonoBehaviour
 {
     static GameManager _instance;
     FishesSpawn _fishSpawn;
-    private float currentTime;
     private float coolTime = 7.5f;
 
     public static GameManager Instance { get { Init(); return _instance; } }
@@ -28,6 +27,7 @@ public class GameManager : MonoBehaviour
 
             PlayerPrefs.SetString("BestAttack", 2.ToString());
             PlayerPrefs.SetInt("BestStage", 1);
+            PlayerPrefs.SetFloat("BestTime", 0);
         }
 
     }
@@ -58,6 +58,7 @@ public class GameManager : MonoBehaviour
             PlayerAttack = 2;
             PlayerScale = 1.0f;
             StageNum = 1;
+            PlayInitTime = -7.0f;
         }
         else
         {
@@ -70,8 +71,9 @@ public class GameManager : MonoBehaviour
     {
         if (PlayMode == 0)//챌린지 모드
         {
-            currentTime += Time.deltaTime;
-            if (currentTime < coolTime) return;
+            PlayInitTime += Time.deltaTime;
+            PlayerMaxTime += Time.deltaTime;
+            if (PlayInitTime < coolTime) return;
 
             StageNum += 1;
             StageNum = (StageNum >= _fishKinds) ? _fishKinds : StageNum;
@@ -79,12 +81,13 @@ public class GameManager : MonoBehaviour
             if (StageNum > PlayerMaxStage)//최고 스테이지 저장
             {
                 PlayerMaxStage = StageNum;
-                PlayerPrefs.SetInt("BestStage", StageNum);
+                PlayerPrefs.SetInt("BestStage", PlayerMaxStage);
             }
-            currentTime = 0.0f;
+            PlayInitTime = 0.0f;
         }
     }
     #region 데이터
+    float _currentTime = -7.0f;
     static int _playMode;
     float _playerMoveSpeed = 4.0f;
     float _playerDir = -1.0f;
@@ -94,18 +97,19 @@ public class GameManager : MonoBehaviour
 
     int _playerMaxStage = 1;
     long _playerMaxAttack = 2;
-    int _playerMaxTime = 0;
+    float _playerMaxTime = 0;
 
     int _stageNumber = 1;
     int _restCount = 30;
     float _enemyMoveSpeed = 2.0f;
     float _enemyToCameraStdDistance = 100.0f;
 
-    const int _fishKinds = 17;
+    const int _fishKinds = 30;
 
     float _bgmVolume = 1.0f;
     float _sfxVolume = 1.0f;
 
+    public float PlayInitTime { get { return _currentTime; }set { _currentTime = value; } }
     public int PlayMode { get { return _playMode; } set { _playMode = value; } }
     public float PlayerMoveSpeed { get { return _playerMoveSpeed; } set { _playerMoveSpeed = value; } }
     public float PlayerDir { get { return _playerDir; } set { _playerDir = _playerDir == 0 ? 1.0f : value; } }
@@ -115,7 +119,7 @@ public class GameManager : MonoBehaviour
 
     public int PlayerMaxStage { get { return _playerMaxStage; } set { _playerMaxStage = value; } }
     public long PlayerMaxAttack { get { return _playerMaxAttack; } set { _playerMaxAttack = value; } }
-    public int PlayerMaxTime { get { return _playerMaxTime; } set { _playerMaxTime = value; } }
+    public float PlayerMaxTime { get { return _playerMaxTime; } set { _playerMaxTime = value; } }
 
     public int StageNum { get { return _stageNumber; } set { _stageNumber = value; } }
     public int RestCount { get { return _restCount; } set { _restCount = value; } }
