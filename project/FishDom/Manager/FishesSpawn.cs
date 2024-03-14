76dde709ee93e08defccc276d2e68fce22aac0ef
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-03-15 01:30:27 +0900

    Update FishesSpawn.cs

--- project/FishDom/Manager/FishesSpawn.cs
+++ project/FishDom/Manager/FishesSpawn.cs
@@ -11,30 +11,32 @@ public class FishesSpawn : MonoBehaviour
     public void Init()
     {
         _objectFull = GameObject.Find("ObjectManager").GetComponent<ObjectFulling>();
+        StartCoroutine(FishSpawnLogic());
+        StartCoroutine(ItemSpawnLogic());
     }
     
     public IEnumerator FishSpawnLogic()
     {
         while (true)
         {
-            int mobCount = UnityEngine.Random.Range(5, 9);
+            int mobCount = UnityEngine.Random.Range(6, 12);
             GameObject[] fishobjects = _objectFull.GetPool(GameManager.Instance.StageNum - 1);
             for (int i = 0; i < mobCount; i++)
             {
-                int spawnX = UnityEngine.Random.Range(2, 6);
-                int spawnY = UnityEngine.Random.Range(2, 6);
+                int spawnX = UnityEngine.Random.Range(2, 7);
+                int spawnY = UnityEngine.Random.Range(2, 7);
                 int spawnMinus = (UnityEngine.Random.Range(-5, 5)>=0)?-1:1;
                 GameObject gm = _objectFull.MakeObj(GameManager.Instance.StageNum - 1);
                 gm.transform.position = new Vector3(target.transform.position.x + spawnX*spawnMinus, target.transform.position.y + spawnY*spawnMinus, 0);
             }
-            yield return new WaitForSeconds(5.0f);
+            yield return new WaitForSeconds(4.5f);
         }
     }
     public IEnumerator ItemSpawnLogic()
     {
         while (true)
         {
-            yield return new WaitForSeconds(18.0f);
+            yield return new WaitForSeconds(20.0f-Time.time/40.0f);
             int spawnX = UnityEngine.Random.Range(3, 7);
             int spawnY = UnityEngine.Random.Range(3, 7);
             int spawnMinus = (UnityEngine.Random.Range(-5, 5) >= 0) ? -1 : 1;
