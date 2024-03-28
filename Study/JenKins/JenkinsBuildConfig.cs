a2f1e3594e5be3be5bf1b38b101cf2310adcbd7b
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-03-28 19:54:38 +0900

    Update JenkinsBuildConfig.cs

--- Study/JenKins/JenkinsBuildConfig.cs
+++ Study/JenKins/JenkinsBuildConfig.cs
@@ -1,23 +1,49 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-namespace Editor
+namespace MGIZMO.Editor
 {
     public class JenkinsBuildConfig : MonoBehaviour
     {
-        [MenuItem("Build/build start")]
-        public static void AutoBuildConfig()
+        private static readonly string licensePath = "C:\\unity-build-result\\Aspose.3DProductFamily.lic";
+        private static readonly string buildDirectoryPath =
+            "C:\\NAVER WORKS Drive\\. Public_Root\\프로젝트@21000000089073\\DSEC_MetaMarine\\99. Build";
+        private static readonly string naverCloudDirectoryPath =
+            "C:\\NAVER WORKS Drive\\. Public_Root\\프로젝트@21000000089073\\DSEC_MetaMarine\\99. Build";
+
+
+        [MenuItem("Build/clean build start")]
+        public static void CleanBuildStart()
         {
+            AutoBuildConfig("clean");
+        }
+        [MenuItem("Build/genernal build start")]
+        public static void GenernalBuildStart()
+        {
+            var buildResultDirectoryPath = AutoBuildConfig("genernal");
+            PressZip.ZipFiles(buildResultDirectoryPath, $"{buildResultDirectoryPath}.zip");
+        }
+
+        public static string AutoBuildConfig(string state)
+        {
+            var year = DateTime.Now.Year;
+            var month = DateTime.Now.Month.ToString("00");
+            var day = DateTime.Now.Day.ToString("00");
+            var nowDirectoryPath = $"{buildDirectoryPath}/{year}{month}{day}/{state}";
+            var projectName = $"Meta-Marine_{year}{month}{day}";
+
             var buildPlayerOptions = new BuildPlayerOptions
             {
-                scenes = new[] { "Assets/Scenes/SampleScene.unity" },
-                locationPathName = $"Builds/Meta-Plant_{PlayerSettings.bundleVersion}.exe",
+                scenes = new[] { "Assets/Metacle UI/Scenes/MainScene.unity" },
+                locationPathName = $"{nowDirectoryPath}/{projectName}.exe",
                 target = BuildTarget.StandaloneWindows64,
                 options = BuildOptions.None
             };
+
             var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
             var summary = report.summary;
 
@@ -35,7 +61,13 @@ namespace Editor
                 default:
                     throw new ArgumentOutOfRangeException();
             }
+            string dataFolderPath = $"{nowDirectoryPath}/{projectName}_Data";
+            if (Directory.Exists(dataFolderPath) && File.Exists(licensePath))
+            {
+                System.IO.File.Copy(licensePath, $"{dataFolderPath}/Aspose.3DProductFamily.lic", true);
+            }
 
+            return nowDirectoryPath;
         }
     }
 }
