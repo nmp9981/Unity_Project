c23a2d27e56339fc2d21be25e9364b056086f989
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-04-07 02:37:54 +0900

    Update GameManager.cs

--- project/BlockRemove/Manager/GameManager.cs
+++ project/BlockRemove/Manager/GameManager.cs
@@ -6,6 +6,10 @@ public class GameManager : MonoBehaviour
 {
     static GameManager _instance;
     
+    public static GameManager Instance
+    {
+        get { Init(); return _instance; }
+    }
     static void Init()
     {
         if (_instance == null)//싱글톤 구현
@@ -26,20 +30,21 @@ public class GameManager : MonoBehaviour
         Init();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     #region 데이터
     int _rowCount = 10;//행
     int _colCount = 20;//열
     int _restBlockCount;//남은 블록 개수
+    int _blockKind = 5;//블록 종류
     int _score = 0;//점수
+    float _bgmVolume;
+    float _sfxVolume;
 
     public int RowCount { get { return _rowCount; } set { _rowCount = value; } }
     public int ColCount { get { return _colCount; } set { _colCount = value; } }
     public int RestBlockCount { get { return _restBlockCount; } set { _restBlockCount = value; } }
+    public int BlockKinds{ get { return _blockKind; } set { _blockKind = value; }}
     public int Score { get { return _score; } set { _score = value; } }
+    public float BGMVolume { get { return _bgmVolume; } set { _bgmVolume = value; } }
+    public float SFXVolume { get { return _sfxVolume; } set { _sfxVolume = value; } }
     #endregion
 }
