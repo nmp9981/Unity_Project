5d9b827ac0cdc4535b1d1f46460e3c747e7fe8b5
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-04-07 02:40:17 +0900

    Update UIManager.cs

--- project/BlockRemove/Manager/UIManager.cs
+++ project/BlockRemove/Manager/UIManager.cs
@@ -11,10 +11,50 @@ public class UIManager : MonoBehaviour
     [SerializeField] public Button _startButton;
     [SerializeField] public GameObject _gameOverUI;
     [SerializeField] public GameObject _getReadyUI;
+    [SerializeField] public GameObject BGMOffImage;
+    [SerializeField] public GameObject SFXOffImage;
 
+    bool _isBGMOn;
+    bool _isSFXOn;
+
+    private void Awake()
+    {
+        _isBGMOn = true;
+        _isSFXOn = true;
+        GameManager.Instance.BGMVolume = 1.0f;
+        GameManager.Instance.SFXVolume = 1.0f;
+    }
     void Update()
     {
         _restBlockCountText.text = $"Count : {GameManager.Instance.RestBlockCount}";
         _scoreText.text = $"Score : {GameManager.Instance.Score}";
+    }
+    public void BGMButton()
+    {
+        _isBGMOn = !_isBGMOn;
+        if (_isBGMOn)
+        {
+            GameManager.Instance.BGMVolume = 1.0f;
+            BGMOffImage.SetActive(false);
+        }
+        else
+        {
+            GameManager.Instance.BGMVolume = 0f;
+            BGMOffImage.SetActive(true);
+        }
+    }
+    public void SFXButton()
+    {
+        _isSFXOn = !_isSFXOn;
+        if (_isSFXOn)
+        {
+            GameManager.Instance.SFXVolume = 1.0f;
+            SFXOffImage.SetActive(false);
+        }
+        else
+        {
+            GameManager.Instance.SFXVolume = 0f;
+            SFXOffImage.SetActive(true);
+        }
     }
 }
