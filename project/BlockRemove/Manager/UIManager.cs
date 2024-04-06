0d125bbc43612a204389636656180edcabb0edaa
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-04-07 02:03:00 +0900

    Update UIManager.cs

--- project/BlockRemove/Manager/UIManager.cs
+++ project/BlockRemove/Manager/UIManager.cs
@@ -7,18 +7,14 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _restBlockCountText;
+    [SerializeField] TextMeshProUGUI _scoreText;
     [SerializeField] public Button _startButton;
     [SerializeField] public GameObject _gameOverUI;
     [SerializeField] public GameObject _getReadyUI;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         _restBlockCountText.text = $"Count : {GameManager.Instance.RestBlockCount}";
+        _scoreText.text = $"Score : {GameManager.Instance.Score}";
     }
 }
