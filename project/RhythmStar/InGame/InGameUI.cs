129011ae76ccfb781b2948c4febd64152ea22a8b
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-04-25 02:11:41 +0900

    Update InGameUI.cs

--- project/RhythmStar/InGame/InGameUI.cs
+++ project/RhythmStar/InGame/InGameUI.cs
@@ -8,7 +8,9 @@ public class InGameUI : MonoBehaviour
     [SerializeField] TextMeshProUGUI _scoreText;
     [SerializeField] TextMeshProUGUI _comboText;
     [SerializeField] TextMeshProUGUI _comboTitle;
+    [SerializeField] TextMeshProUGUI _comboBonusText;
 
+    int[] bounusCut = new int[4]{ 20,40,70,100};
     // Start is called before the first frame update
     void Start()
     {
@@ -37,24 +39,33 @@ public class InGameUI : MonoBehaviour
     }
     void ComboBonusJudge()
     {
-        if (GameManager.Instance.ComboCount < 20)
+        if (GameManager.Instance.ComboCount < bounusCut[0])
         {
             GameManager.Instance.ComboBonus = 1;
-        }else if(GameManager.Instance.ComboCount < 40 && GameManager.Instance.ComboCount >= 20)
+            _comboBonusText.text = "";
+        }else if(GameManager.Instance.ComboCount < bounusCut[1] && GameManager.Instance.ComboCount >= bounusCut[0])
         {
             GameManager.Instance.ComboBonus = 2;
+            _comboBonusText.text = "X2";
+            _comboBonusText.color = Color.blue;
         }
-        else if (GameManager.Instance.ComboCount < 70 && GameManager.Instance.ComboCount >= 40)
+        else if (GameManager.Instance.ComboCount < bounusCut[2] && GameManager.Instance.ComboCount >= bounusCut[1])
         {
             GameManager.Instance.ComboBonus = 3;
+            _comboBonusText.text = "X3";
+            _comboBonusText.color = Color.green;
         }
-        else if (GameManager.Instance.ComboCount < 100 && GameManager.Instance.ComboCount >= 70)
+        else if (GameManager.Instance.ComboCount < bounusCut[3] && GameManager.Instance.ComboCount >= bounusCut[2])
         {
             GameManager.Instance.ComboBonus = 4;
+            _comboBonusText.text = "X4";
+            _comboBonusText.color = Color.red;
         }
-        else if (GameManager.Instance.ComboCount >= 100)
+        else if (GameManager.Instance.ComboCount >= bounusCut[3])
         {
             GameManager.Instance.ComboBonus = 5;
+            _comboBonusText.text = "X5";
+            _comboBonusText.color = Color.magenta;
         }
     }
 }
