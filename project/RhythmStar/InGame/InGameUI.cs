61d903573e6187cb69dd57e7f9213f4636680489
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-04-25 01:55:50 +0900

    Update InGameUI.cs

--- project/RhythmStar/InGame/InGameUI.cs
+++ project/RhythmStar/InGame/InGameUI.cs
@@ -18,6 +18,7 @@ public class InGameUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ComboBonusJudge();
         ShowText();
     }
     void ShowText()
@@ -32,6 +33,28 @@ public class InGameUI : MonoBehaviour
         {
             _comboText.text = "";
             _comboTitle.text = "";
+        }
+    }
+    void ComboBonusJudge()
+    {
+        if (GameManager.Instance.ComboCount < 20)
+        {
+            GameManager.Instance.ComboBonus = 1;
+        }else if(GameManager.Instance.ComboCount < 40 && GameManager.Instance.ComboCount >= 20)
+        {
+            GameManager.Instance.ComboBonus = 2;
+        }
+        else if (GameManager.Instance.ComboCount < 70 && GameManager.Instance.ComboCount >= 40)
+        {
+            GameManager.Instance.ComboBonus = 3;
+        }
+        else if (GameManager.Instance.ComboCount < 100 && GameManager.Instance.ComboCount >= 70)
+        {
+            GameManager.Instance.ComboBonus = 4;
+        }
+        else if (GameManager.Instance.ComboCount >= 100)
+        {
+            GameManager.Instance.ComboBonus = 5;
         }
     }
 }
