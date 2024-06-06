ae7b3b74fa63393a830e31ff814c54fc3a2b5833
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-06-06 16:03:41 +0900

    Create ObjectFulling.cs

---
+++ project/EverPlanet/GameManager/ObjectFulling.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFulling : MonoBehaviour
+{
+    //프리팹 준비
+    const int blockMaxCount = 70;
+    const int blockKinds = 4;
+    public GameObject[] blockPrefabs;
+
+    //오브젝트 배열
+    GameObject[][] blocks;
+    GameObject[] targetPool;
+
+    void Awake()
+    {
+        blocks = new GameObject[blockKinds][]
+        {
+             new GameObject[blockMaxCount],
+             new GameObject[blockMaxCount],
+             new GameObject[blockMaxCount],
+             new GameObject[blockMaxCount]
+        };
+        Generate();
+    }
+    void Generate()
+    {
+        //블록
+        for (int i = 0; i < blockKinds; i++)
+        {
+            for (int j = 0; j < blockMaxCount; j++)
+            {
+                blocks[i][j] = Instantiate(blockPrefabs[i]);
+                blocks[i][j].SetActive(false);
+            }
+        }
+    }
+    //오브젝트 생성
+    public GameObject MakeObj(int num)
+    {
+        targetPool = blocks[num];
+
+        for (int i = 0; i < targetPool.Length; i++)
+        {
+            if (!targetPool[i].activeSelf)
+            {
+                targetPool[i].SetActive(true);//활성화 후 넘김
+                return targetPool[i];
+            }
+        }
+        return null;//없으면 빈 객체
+    }
+    //오브젝트 배열 가져오기
+    public GameObject[] GetPool(int num)//지정한 오브젝트 풀 가져오기
+    {
+        targetPool = blocks[num];
+        return targetPool;
+    }
+    //오브젝트들 비활성화
+    public void OffObj()
+    {
+        for (int i = 0; i < blockKinds; i++)
+        {
+            for (int j = 0; j < blockMaxCount; j++)
+            {
+                if (blocks[i][j].activeSelf) blocks[i][j].SetActive(false);
+            }
+        }
+    }
+}
