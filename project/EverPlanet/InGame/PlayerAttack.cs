e763ec8d4904293960278a6d6354e12bd508e50f
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-06-08 04:26:11 +0900

    Update PlayerAttack.cs

--- project/EverPlanet/InGame/PlayerAttack.cs
+++ project/EverPlanet/InGame/PlayerAttack.cs
@@ -5,28 +5,43 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     ObjectFulling objectfulling;
-    [SerializeField]
-    Transform startDragPosition;
+    [SerializeField] Transform startDragPosition;
+    [SerializeField] Animator anim;
+
+    float curTime = 0;
+    float coolTime;
     void Awake()
     {
-        objectfulling = GameObject.Find("ObjectManager").GetComponent<ObjectFulling>(); 
+        objectfulling = GameObject.Find("ObjectManager").GetComponent<ObjectFulling>();
+        coolTime = GameManager.Instance.PlayerAttackSpeed;
     }
 
     void Update()
-    { 
-        if (Input.GetKeyDown(KeyCode.X))
+    {
+        Attack();
+        TimeFlow();
+    }
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.X) && curTime >= coolTime)
         {
             StartCoroutine(ShotDrag());
+            curTime = 0;
         }
-        
+    }
+    void TimeFlow()
+    {
+        curTime += Time.deltaTime;
     }
     IEnumerator ShotDrag()
     {
-        for (int i = 0; i < 2; i++)
+        anim.SetBool("BasicAttack", true);
+        for (int i = 0; i < 3; i++)
         {
             GameObject gm = objectfulling.MakeObj(2);
             gm.transform.position = startDragPosition.transform.position;//캐릭터 위치에서 날리기 시작
             yield return new WaitForSeconds(0.2f);
         }
+        anim.SetBool("BasicAttack", false);
     }
 }
