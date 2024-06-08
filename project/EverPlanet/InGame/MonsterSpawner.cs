ce3b080ee8fb78759d70fdc67a9af8174e9809b6
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-06-08 17:52:54 +0900

    Update MonsterSpawner.cs

--- project/EverPlanet/InGame/MonsterSpawner.cs
+++ project/EverPlanet/InGame/MonsterSpawner.cs
@@ -6,21 +6,36 @@ public class MonsterSpawner : MonoBehaviour
 {
     public int mobCount;
     ObjectFulling objectfulling;
+
+    float curTime = 0;
+    float coolTime = 8.0f;
     void Awake()
     {
-        InvokeRepeating("MonsterSpawn", 0.5f, 8f);
         objectfulling = GameObject.Find("ObjectManager").GetComponent<ObjectFulling>();
         mobCount = 0;
         GameManager.Instance.MaxMonsterCount = 18;
     }
-    void MonsterSpawn()
+    private void Update()
     {
-        for(int i = 0; i < 5; i++)
+        if (curTime >= coolTime)
         {
-            if (mobCount >= GameManager.Instance.MaxMonsterCount) return;
+            StartCoroutine(MonsterSpawn());
+            curTime = 0;
+        }
+        TimeFlow();
+    }
+    void TimeFlow()
+    {
+        curTime += Time.deltaTime;
+    }
+    IEnumerator MonsterSpawn()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (mobCount >= GameManager.Instance.MaxMonsterCount) yield break;
             int monsterNumber = Random.Range(4, 6);
             GameObject gm = objectfulling.MakeObj(monsterNumber);
-            gm.transform.position = new Vector3(Random.Range(-9,9),-1f,Random.Range(-9,9));
+            gm.transform.position = new Vector3(Random.Range(-9, 9), -1f, Random.Range(-9, 9));
             mobCount++;
         }
     }
