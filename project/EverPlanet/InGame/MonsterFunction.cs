c1db5c772c21c5dcd0eae24347ede1e9cb6228cb
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-06-13 03:37:24 +0900

    Update MonsterFunction.cs

--- project/EverPlanet/InGame/MonsterFunction.cs
+++ project/EverPlanet/InGame/MonsterFunction.cs
@@ -14,6 +14,7 @@ public class MonsterFunction : MonoBehaviour
     [SerializeField] Image monsterHPBarBack;
     [SerializeField] Image monsterHPBar;
     [SerializeField] TextMeshProUGUI monsterInfo;
+    [SerializeField] TextMeshProUGUI[] hitDamage;
     void Awake()
     {
         monsterSpawner = GameObject.Find("MonsterSpawn").GetComponent<MonsterSpawner>();
@@ -21,6 +22,7 @@ public class MonsterFunction : MonoBehaviour
     private void OnEnable()
     {
         monsterHP = monsterFullHP;
+        foreach (var damage in hitDamage) damage.text = "";
     }
     void Update()
     {
@@ -33,6 +35,8 @@ public class MonsterFunction : MonoBehaviour
         monsterHPBar.transform.position = Camera.main.WorldToScreenPoint(this.gameObject.transform.position + new Vector3(0, 1f, 0));
         monsterInfo.transform.position = Camera.main.WorldToScreenPoint(this.gameObject.transform.position + new Vector3(0, 1.5f, 0));
 
+        for (int idx = 0; idx < hitDamage.Length; idx++) hitDamage[idx].transform.position = Camera.main.WorldToScreenPoint(this.gameObject.transform.position + new Vector3(0, idx+2f, 0));
+
         monsterHPBar.fillAmount = (float)monsterHP / (float)monsterFullHP;
         monsterInfo.text = name;
     }
@@ -44,5 +48,26 @@ public class MonsterFunction : MonoBehaviour
             MonsterSpawner.spawnMonster.Remove(this.gameObject);
             gameObject.SetActive(false);
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Weapon")
+        {
+            for(int idx = 0; idx < hitDamage.Length; idx++)
+            {
+                if (hitDamage[idx].text == "")
+                {
+                    StartCoroutine(ShowDamage(hitDamage[idx],idx));
+                    return;
+                }
+            }
+        }
+    }
+    //데미지 보여주기
+    IEnumerator ShowDamage(TextMeshProUGUI damage, int idx)
+    {
+        damage.text = GameManager.Instance.PlayerAttack.ToString();
+        yield return new WaitForSeconds(0.5f);
+        damage.text = "";
     }
 }
