0d32b5beb4f45f72510715893aefbe22bcbdd920
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-06-19 02:46:52 +0900

    Update PlayerHit.cs

--- project/EverPlanet/InGame/Player/PlayerHit.cs
+++ project/EverPlanet/InGame/Player/PlayerHit.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerHit : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI hitDamageText;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        hitDamageText.text = "";
     }
 
     // Update is called once per frame
@@ -20,7 +22,18 @@ public class PlayerHit : MonoBehaviour
         if(collision.gameObject.tag == "Monster")
         {
             int hit = collision.gameObject.GetComponent<MonsterFunction>().monsterHitDamage;
-            GameManager.Instance.PlayerHP -= hit;
+            int finalHit = Random.Range(hit * 90 / 100, hit * 110 / 100);
+            GameManager.Instance.PlayerHP -= finalHit;
+            StartCoroutine(ShowDamage(finalHit));
         }
+    }
+    //데미지 보여주기
+    IEnumerator ShowDamage(int finalHit)
+    {
+        hitDamageText.transform.position = Camera.main.WorldToScreenPoint(this.gameObject.transform.position + new Vector3(0, 2f, 0));
+        Debug.Log(finalHit);
+        hitDamageText.text = finalHit.ToString();
+        yield return new WaitForSeconds(0.3f);
+        hitDamageText.text = "";
     }
 }
