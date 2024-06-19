911575af73c15e5907d8b3bfab3663d52e8998e6
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-06-20 03:12:17 +0900

    Update GameManager.cs

--- project/EverPlanet/GameManager/GameManager.cs
+++ project/EverPlanet/GameManager/GameManager.cs
@@ -32,13 +32,16 @@ public class GameManager : MonoBehaviour
     
     #region 데이터
     float _playerMoveSpeed = 4.0f;//기본 이속
-    float _playerJumpSpeed = 4.0f;//기본 점프력
+    float _playerJumpSpeed = 6.0f;//기본 점프력
     int _maxJumpCount = 2;//최대 점프 횟수
     
     bool _playerHit = true;//플레이어 피격 여부
-    long _playerAttack = 2;//플레이어 공격력
+    long _playerAttack = 450;//플레이어 공격력
     float _playerDragSpeed = 12.0f;//표창 날아가는 속도
     float _playerAttackSpeed = 0.5f;//플레이어 공격 속도
+    long _shadowAttack = 50;//그림자 공격력
+
+    int _workmanship = 50;
 
     string _playerJob;
     int _playerLV;
@@ -48,6 +51,12 @@ public class GameManager : MonoBehaviour
     int _playerMaxMP;
     long _playerExp;
     long _playerReqExp;
+    int _playerDex;
+    int _playerLuk;
+    int _playerAcc;
+
+    int _hpPosionCount;
+    int _mpPosionCount;
 
     int _maxMonsterCount;//최대 스폰 몬스터 수
 
@@ -62,6 +71,9 @@ public class GameManager : MonoBehaviour
     public long PlayerAttack { get { return _playerAttack; } set { _playerAttack = value; } }
     public float PlayerDragSpeed { get { return _playerDragSpeed; } set { _playerDragSpeed = value; } }
     public float PlayerAttackSpeed { get { return _playerAttackSpeed; } set { _playerAttackSpeed = value; } }
+    public long ShadowAttack { get { return _shadowAttack; } set { _shadowAttack = value; } }
+
+    public int Workmanship { get { return _workmanship; } set { _workmanship = value; } }
 
     public string PlayerJob { get { return _playerJob; } set { _playerJob = value; } }
     public int PlayerLV { get { return _playerLV; } set { _playerLV = value; } }
@@ -71,6 +83,12 @@ public class GameManager : MonoBehaviour
     public int PlayerMaxMP { get { return _playerMaxMP; } set { _playerMaxMP = value; } }
     public long PlayerEXP { get { return _playerExp; } set { _playerExp = value; } }
     public long PlayerReqExp { get { return _playerReqExp; } set { _playerReqExp = value; } }
+    public int PlayerDEX { get { return _playerDex; } set { _playerDex = value; } }
+    public int PlayerLUK { get { return _playerLuk; } set { _playerLuk = value; } }
+    public int PlayerACC { get { return _playerAcc; } set { _playerAcc = value; } }
+
+    public int HPPosionCount { get { return _hpPosionCount; } set { _hpPosionCount = value; } }
+    public int MPPosionCount { get { return _mpPosionCount; } set { _mpPosionCount = value; } }
 
     public int MaxMonsterCount { get { return _maxMonsterCount; } set { _maxMonsterCount = value; } }
 
