e4f99886f85264069d463f5fc0c4567358198443
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-06-20 02:40:08 +0900

    Update InGameUI.cs

--- project/EverPlanet/InGame/InGameUI.cs
+++ project/EverPlanet/InGame/InGameUI.cs
@@ -14,10 +14,23 @@ public class InGameUI : MonoBehaviour
     [SerializeField] Image playerHPFill;
     [SerializeField] Image playerMPFill;
     [SerializeField] Image playerExpFill;
- 
+
+    [SerializeField] TextMeshProUGUI curHPPosionCount;
+    [SerializeField] TextMeshProUGUI curMPPosionCount;
+
+    [Header ("Stat")] 
+    [SerializeField] GameObject statUI;
+    [SerializeField] TextMeshProUGUI statJobText;
+    [SerializeField] TextMeshProUGUI statLvText;
+    [SerializeField] TextMeshProUGUI statHPText;
+    [SerializeField] TextMeshProUGUI statMPText;
+    [SerializeField] TextMeshProUGUI statEXPText;
+
     void Update()
     {
         ShowPlayerUI();
+        ShowStatUI();
+        StatUIInfo();
     }
     void ShowPlayerUI()
     {
@@ -32,5 +45,24 @@ public class InGameUI : MonoBehaviour
         float expRate = (float)GameManager.Instance.PlayerEXP*100 / (float)GameManager.Instance.PlayerReqExp;
         playerExpText.text = string.Format("Exp. {0} / {1} [{2:N2}%]", GameManager.Instance.PlayerEXP, GameManager.Instance.PlayerReqExp, expRate);
         playerExpFill.fillAmount = (float)GameManager.Instance.PlayerEXP / (float) GameManager.Instance.PlayerReqExp;
+
+        curHPPosionCount.text = $"{GameManager.Instance.HPPosionCount}";
+        curMPPosionCount.text = $"{GameManager.Instance.MPPosionCount}";
+    }
+    void ShowStatUI()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (statUI.activeSelf) statUI.SetActive(false);
+            else statUI.SetActive(true);
+        }
+    }
+    void StatUIInfo()
+    {
+        statJobText.text = $"{GameManager.Instance.PlayerJob}";
+        statLvText.text = $"{GameManager.Instance.PlayerLV}";
+        statHPText.text = $"{GameManager.Instance.PlayerHP} / {GameManager.Instance.PlayerMaxHP}";
+        statMPText.text = $"{GameManager.Instance.PlayerMP} / {GameManager.Instance.PlayerMaxMP}";
+        statEXPText.text = $"{GameManager.Instance.PlayerEXP} [{GameManager.Instance.PlayerEXP*100/ GameManager.Instance.PlayerReqExp}%]";
     }
 }
