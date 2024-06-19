ef36544058c17ed8951d64ee26f6e97de30fe291
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-06-20 02:42:24 +0900

    Update PlayerHeal.cs

--- project/EverPlanet/InGame/Player/PlayerHeal.cs
+++ project/EverPlanet/InGame/Player/PlayerHeal.cs
@@ -25,12 +25,14 @@ public class PlayerHeal : MonoBehaviour
     }
     void HealHP()
     {
+        if (GameManager.Instance.HPPosionCount <= 0) return;
         int healHPAmount = 1000;
         GameManager.Instance.PlayerHP = Mathf.Min(GameManager.Instance.PlayerHP+ healHPAmount, GameManager.Instance.PlayerMaxHP);
         GameManager.Instance.HPPosionCount -= 1;
     }
     void HealMP()
     {
+        if (GameManager.Instance.MPPosionCount <= 0) return;
         int healMPAmount = 300;
         GameManager.Instance.PlayerMP = Mathf.Min(GameManager.Instance.PlayerMP + healMPAmount, GameManager.Instance.PlayerMaxMP);
         GameManager.Instance.MPPosionCount -= 1;
