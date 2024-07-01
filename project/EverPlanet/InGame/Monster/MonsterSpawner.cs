db8977a96c7fff842f98632d613ae7d8efa479c6
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-07-02 03:20:41 +0900

    Update MonsterSpawner.cs

--- project/EverPlanet/InGame/Monster/MonsterSpawner.cs
+++ project/EverPlanet/InGame/Monster/MonsterSpawner.cs
@@ -9,7 +9,7 @@ public class MonsterSpawner : MonoBehaviour
 
     [SerializeField] List<Transform> spawnPositionList;
 
-    float curTime = 0;
+    float curTime = 6.5f;
     float coolTime = 7.0f;
 
     static public List<GameObject> spawnMonster = new List<GameObject>();
@@ -18,7 +18,7 @@ public class MonsterSpawner : MonoBehaviour
         objectfulling = GameObject.Find("ObjectManager").GetComponent<ObjectFulling>();
         mobCount = new int[2] {0,0};
         curTime = 7.0f;
-        GameManager.Instance.MaxMonsterCount = 24;
+        GameManager.Instance.MaxMonsterCount = 30;
     }
     private void Update()
     {
@@ -37,12 +37,13 @@ public class MonsterSpawner : MonoBehaviour
     {
         int spawnPositionNumber = Random.Range(0,7);
         int mapNum = MapNumber(spawnPositionNumber);
-        for (int i = 0; i < 5; i++)
+        int spawnCount = Random.Range(4,8);//스폰 마릿수
+        for (int i = 0; i < spawnCount; i++)
         {
             if (mobCount[mapNum] >= GameManager.Instance.MaxMonsterCount) yield break;
             int monsterNumber = MonsterPosition(spawnPositionNumber,mapNum);//스폰할 몬스터
             GameObject gm = objectfulling.MakeObj(monsterNumber);
-            gm.transform.position = spawnPositionList[spawnPositionNumber].position +new Vector3(Random.Range(-9, 9), 1f, Random.Range(-9, 9));
+            gm.transform.position = spawnPositionList[spawnPositionNumber].position +new Vector3(Random.Range(-7, 7), 1f, Random.Range(-7, 7));
             spawnMonster.Add(gm);
         }
     }
@@ -66,11 +67,11 @@ public class MonsterSpawner : MonoBehaviour
         }
         else if (spawnPositionNumber == 4)
         {
-            return 5;
+            return 6;
         }
         else if (spawnPositionNumber >= 5 && spawnPositionNumber <= 6)
         {
-            return Random.Range(5, 7);
+            return Random.Range(6, 8);
         }
         return 0;
     }
