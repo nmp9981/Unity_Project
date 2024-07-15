8664778b859555af02e3109ebe3d21d6c4eb1842
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-07-16 03:08:02 +0900

    Update ItemManager.cs

--- project/EverPlanet/GameManager/ItemManager.cs
+++ project/EverPlanet/GameManager/ItemManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] UserItemList = new GameObject[24];
-    void Start()
+    public List<Item> UserItemList = new List<Item>();
+    public GameObject[] UserHaveItem = new GameObject[20];
+
+    private void Awake()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        ShowItemIcon();
+    }
+    //아이템 창 키면 보임
+    void ShowItemIcon()
+    {
+        foreach(var item in UserItemList)
+        {
+            if (item.theNumber > 0)
+            {
+                for(int idx=0;idx<20;idx++)
+                {
+                    if (!UserHaveItem[idx].activeSelf)
+                    {
+                        UserHaveItem[idx].SetActive(true);
+                        //UserHaveItem[idx].GetComponent<Image>() = itemImageList[item];
+                        UserHaveItem[idx].GetComponentInChildren<TextMeshProUGUI>().text = item.theNumber.ToString();
+                        break;
+                    }
+                   
+                }
+            }
+        }
     }
 }
