8ddc6a2697573bfbe9c247e56564ee5ade11c9b0
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-07-15 23:54:59 +0900

    Create ItemManager.cs

---
+++ project/EverPlanet/GameManager/ItemManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] UserItemList = new GameObject[24];
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
