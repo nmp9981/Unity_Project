f2cb89c7be4d6c8dab48d2ba2314ba6653309630
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-07-17 03:10:02 +0900

    Update ItemManager.cs

--- project/EverPlanet/GameManager/ItemManager.cs
+++ project/EverPlanet/GameManager/ItemManager.cs
@@ -3,37 +3,49 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class ItemManager : MonoBehaviour
 {
-    public List<Item> UserItemList = new List<Item>();
+    public Item[] UserItemList = new Item[11];
     public GameObject[] UserHaveItem = new GameObject[20];
-
+    public List<GameObject> UserHaveItemList = new List<GameObject>();
     private void Awake()
     {
-        
+        EmptyItemShow();
     }
     void Update()
     {
         ShowItemIcon();
     }
+    void EmptyItemShow()
+    {
+        for (int idx = 0; idx < 20; idx++)
+        {
+            UserHaveItem[idx].SetActive(false);
+        }
+        
+    }
     //아이템 창 키면 보임
     void ShowItemIcon()
     {
-        foreach(var item in UserItemList)
+        foreach(var item in GameManager.Instance.storeItemList)
         {
             if (item.theNumber > 0)
             {
+                Debug.Log(item.itemIdx + " " + item.theNumber);
+
                 for(int idx=0;idx<20;idx++)
                 {
+                    if (UserHaveItem[idx].activeSelf) continue;
                     if (!UserHaveItem[idx].activeSelf)
                     {
                         UserHaveItem[idx].SetActive(true);
-                        //UserHaveItem[idx].GetComponent<Image>() = itemImageList[item];
+                        UserHaveItem[idx].GetComponent<Image>().sprite = item.itemSpriteImage;
                         UserHaveItem[idx].GetComponentInChildren<TextMeshProUGUI>().text = item.theNumber.ToString();
-                        break;
+                        return;
                     }
-                   
+                    
                 }
             }
         }
