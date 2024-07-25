a2123377dbf9c5ad2a1f228d9d6596843e5eae1f
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-07-26 03:25:31 +0900

    Update Stome.cs

--- project/EverPlanet/InGame/Monster/Boss/Stome.cs
+++ project/EverPlanet/InGame/Monster/Boss/Stome.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class Stome : MonoBehaviour
 {
     Vector3 target;
-   
+    bool isAttack;
     public void Init()
     {
         GameObject circle = GameObject.Find("Circle");
         target = circle.transform.position;
         circle.SetActive(false);
         gameObject.transform.position = new Vector3(0,1,0);
+        isAttack = true;
     }
     void Update()
     {
-        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, target, Time.deltaTime);
+        if (isAttack)
+        {
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, target, Time.deltaTime);
+        }
     }
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        isAttack = false;
+    }
 }
