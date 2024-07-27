b36fb50f98804e1f4102513b6b809a4312c01b51
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-07-28 04:46:49 +0900

    Update Stome.cs

--- project/EverPlanet/InGame/Monster/Boss/Stome.cs
+++ project/EverPlanet/InGame/Monster/Boss/Stome.cs
@@ -1,32 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Stome : MonoBehaviour
 {
     Vector3 dir;
+    Vector3 circlePos;
+    Vector3 returnPos;
     bool isAttack;
+    GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+        gameObject.transform.position = new Vector3(100,50,0);
+    }
     public void Init(Vector3 targetPos, GameObject circle, Vector3 bossPos)
     {
+        gameObject.SetActive(true);
+        circlePos = targetPos;
         circle.SetActive(false);
-        gameObject.transform.position = bossPos;
         isAttack = true;
-        dir = (targetPos-bossPos).normalized;
-        InvokeRepeating("StoneMove", 0.05f, 0.1f);
+        returnPos = circlePos+new Vector3(0,30,0);
+        gameObject.transform.position = returnPos;
     }
-    void StoneMove()
+    private void Update()
     {
-        if (isAttack)
-        {
-            gameObject.transform.position += dir;
-            //gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, target, 1f);
-        }
+        if(isAttack) gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, circlePos, 10f);
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player")
         {
+            int damage = Random.Range(2400, 2850);
+            GameManager.Instance.PlayerHP -= damage;
+            if (GameManager.Instance.PlayerHP <= 0) GameManager.Instance.PlayerHP = 0;
+            StartCoroutine(player.GetComponent<PlayerHit>().ShowDamage(damage));
             isAttack = false;
+            Invoke("InitStone", 0.01f);
         }
+        if (collision.gameObject.tag == "Ground") Invoke("InitStone", 1f);
+    }
+    void InitStone()
+    {
+        gameObject.transform.position = returnPos;
+        gameObject.SetActive(false);
     }
 }
