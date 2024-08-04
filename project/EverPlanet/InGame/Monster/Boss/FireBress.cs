ad1e09bfa2c9e36455ad589568ca0e5333cc40ae
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-08-05 03:57:02 +0900

    Update FireBress.cs

--- project/EverPlanet/InGame/Monster/Boss/FireBress.cs
+++ project/EverPlanet/InGame/Monster/Boss/FireBress.cs
@@ -47,14 +47,4 @@ public class FireBress : MonoBehaviour
             }
         }
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Player" && !GameManager.Instance.IsInvincibility)
-        {
-            int damage = UnityEngine.Random.Range(4700, 5400);
-            GameManager.Instance.PlayerHP -= damage;
-            if (GameManager.Instance.PlayerHP <= 0) GameManager.Instance.PlayerHP = 0;
-            StartCoroutine(collision.gameObject.GetComponent<PlayerHit>().ShowDamage(damage));
-        }
-    }
 }
