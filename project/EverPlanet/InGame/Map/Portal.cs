832d2e022b801c13293795bcfd55380f590bf20e
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-08-10 12:57:11 +0900

    Update Portal.cs

--- project/EverPlanet/InGame/Map/Portal.cs
+++ project/EverPlanet/InGame/Map/Portal.cs
@@ -27,27 +27,35 @@ public class Portal : MonoBehaviour
         {
             case int n when(n<=2):
                 GameManager.Instance.PlayerCurrentMap = 0;
+                SoundManager._sound.PlayBGM(0);
                 break;
             case int n when (n>=3 && n <= 4):
                 GameManager.Instance.PlayerCurrentMap = 1;
+                SoundManager._sound.PlayBGM(0);
                 break;
             case int n when (n >= 5 && n <= 7):
                 GameManager.Instance.PlayerCurrentMap = 2;
+                SoundManager._sound.PlayBGM(1);
                 break;
             case int n when (n >= 8 && n <= 9):
                 GameManager.Instance.PlayerCurrentMap = 3;
+                SoundManager._sound.PlayBGM(2);
                 break;
             case int n when (n >= 10 && n <= 11):
                 GameManager.Instance.PlayerCurrentMap = 4;
+                SoundManager._sound.PlayBGM(3);
                 break;
             case int n when (n >= 12 && n <= 13):
                 GameManager.Instance.PlayerCurrentMap = 5;
+                SoundManager._sound.PlayBGM(4);
                 break;
             case int n when (n >= 14 && n <= 15):
                 GameManager.Instance.PlayerCurrentMap = 6;
+                SoundManager._sound.PlayBGM(5);
                 break;
             case 16:
                 GameManager.Instance.PlayerCurrentMap = 7;
+                SoundManager._sound.PlayBGM(6);
                 break;
         }
     }
