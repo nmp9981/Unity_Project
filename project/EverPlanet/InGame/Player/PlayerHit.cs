dd6ac020f0aec98b1d10766d22a1d4dea2729659
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-08-12 02:41:38 +0900

    Update PlayerHit.cs

--- project/EverPlanet/InGame/Player/PlayerHit.cs
+++ project/EverPlanet/InGame/Player/PlayerHit.cs
@@ -74,8 +74,11 @@ public class PlayerHit : MonoBehaviour
         GameManager.Instance.PlayerEXP = Mathf.Max(0, (int)GameManager.Instance.PlayerEXP-(int)GameManager.Instance.PlayerReqExp /20);
 
         this.gameObject.transform.position = PortalManager.PortalInstance.portalist[0].transform.position;//마을로 이동
+
+        SoundManager._sound.StopBGM(GameManager.Instance.PlayerBGMNumber);
+        GameManager.Instance.PlayerBGMNumber = 0;
         GameManager.Instance.PlayerCurrentMap = 0;
-        SoundManager._sound.PlayBGM(0);
+        SoundManager._sound.PlayBGM(GameManager.Instance.PlayerBGMNumber);
 
         tombStone.SetActive(false);
         tombStoneMessage.SetActive(false);
