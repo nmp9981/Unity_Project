7523c4b295f036ebda88d802436c6d96eaee20b8
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-08-11 22:45:48 +0900

    Update Portal.cs

--- project/EverPlanet/InGame/Map/Portal.cs
+++ project/EverPlanet/InGame/Map/Portal.cs
@@ -23,44 +23,51 @@ public class Portal : MonoBehaviour
     }
     void ChangeMap(int nextPortalNum)
     {
+        int beforeBGMNymber = GameManager.Instance.PlayerBGMNumber;//이전 bgm 번호
         switch (nextPortalNum)
         {
             case 0:
                 GameManager.Instance.PlayerCurrentMap = 0;
-                SoundManager._sound.PlayBGM(0);
+                GameManager.Instance.PlayerBGMNumber = 0;
                 break;
             case int n when(n>=1 && n<=2):
                 GameManager.Instance.PlayerCurrentMap = 1;
-                SoundManager._sound.PlayBGM(0);
+                GameManager.Instance.PlayerBGMNumber = 0;
                 break;
             case int n when (n>=3 && n <= 4):
                 GameManager.Instance.PlayerCurrentMap = 2;
-                SoundManager._sound.PlayBGM(0);
+                GameManager.Instance.PlayerBGMNumber = 0;
                 break;
             case int n when (n >= 5 && n <= 7):
                 GameManager.Instance.PlayerCurrentMap = 3;
-                SoundManager._sound.PlayBGM(1);
+                GameManager.Instance.PlayerBGMNumber = 1;
                 break;
             case int n when (n >= 8 && n <= 9):
                 GameManager.Instance.PlayerCurrentMap = 4;
-                SoundManager._sound.PlayBGM(2);
+                GameManager.Instance.PlayerBGMNumber = 2;
                 break;
             case int n when (n >= 10 && n <= 11):
                 GameManager.Instance.PlayerCurrentMap = 5;
-                SoundManager._sound.PlayBGM(3);
+                GameManager.Instance.PlayerBGMNumber = 3;
                 break;
             case int n when (n >= 12 && n <= 13):
                 GameManager.Instance.PlayerCurrentMap = 6;
-                SoundManager._sound.PlayBGM(4);
+                GameManager.Instance.PlayerBGMNumber = 4;
                 break;
             case int n when (n >= 14 && n <= 15):
                 GameManager.Instance.PlayerCurrentMap = 7;
-                SoundManager._sound.PlayBGM(5);
+                GameManager.Instance.PlayerBGMNumber = 5;
                 break;
             case 16:
                 GameManager.Instance.PlayerCurrentMap = 8;
-                SoundManager._sound.PlayBGM(6);
+                GameManager.Instance.PlayerBGMNumber = 6;
                 break;
+        }
+        //새로운 BGM 재생
+        if(beforeBGMNymber != GameManager.Instance.PlayerBGMNumber)
+        {
+            SoundManager._sound.StopBGM(beforeBGMNymber);
+            SoundManager._sound.PlayBGM(GameManager.Instance.PlayerBGMNumber);
         }
     }
 }
