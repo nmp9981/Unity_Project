dd65b9ee3ee1a3a6a17dd7f38580d21f00f114c6
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-08-25 14:11:20 +0900

    Create DataSave.cs

---
+++ project/EverPlanet/GameManager/DataSave.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class DataSave : MonoBehaviour
+{
+    int currentPlayerLv;
+    void Awake()
+    {
+        //DataGet();
+    }
+    void Update()
+    {
+        //DataSet();
+    }
+    void DataGet()
+    {
+        if (PlayerPrefs.HasKey("Lv")) GameManager.Instance.PlayerLV = PlayerPrefs.GetInt("Lv");
+        else GameManager.Instance.PlayerLV = 10;
+    }
+    void DataSet()
+    {
+        PlayerPrefs.SetInt("Lv", GameManager.Instance.PlayerLV);
+    }
+}
