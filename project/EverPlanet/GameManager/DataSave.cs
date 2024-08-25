752909db0a7f717cbd5eb20b21f5148c6226e611
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-08-25 16:05:20 +0900

    Update DataSave.cs

--- project/EverPlanet/GameManager/DataSave.cs
+++ project/EverPlanet/GameManager/DataSave.cs
@@ -18,9 +18,19 @@ public class DataSave : MonoBehaviour
     {
         if (PlayerPrefs.HasKey("Lv")) GameManager.Instance.PlayerLV = PlayerPrefs.GetInt("Lv");
         else GameManager.Instance.PlayerLV = 10;
+        if (PlayerPrefs.HasKey("Meso")) GameManager.Instance.PlayerMeso = PlayerPrefs.GetInt("Meso");
+        else GameManager.Instance.PlayerMeso = 10000;
+        
+        if (PlayerPrefs.HasKey("AP")) GameManager.Instance.ApPoint = PlayerPrefs.GetInt("AP");
+        else GameManager.Instance.ApPoint = 0;
+        if (PlayerPrefs.HasKey("SP")) GameManager.Instance.SkillPoint = PlayerPrefs.GetInt("SP");
+        else GameManager.Instance.SkillPoint = 0;
     }
     void DataSet()
     {
         PlayerPrefs.SetInt("Lv", GameManager.Instance.PlayerLV);
+        PlayerPrefs.SetInt("Meso", GameManager.Instance.PlayerMeso);
+        PlayerPrefs.SetInt("AP", GameManager.Instance.ApPoint);
+        PlayerPrefs.SetInt("SP", GameManager.Instance.SkillPoint);
     }
 }
