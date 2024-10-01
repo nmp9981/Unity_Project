9060afb3d9073a2eba95cf6df4a92786cabce61b
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-10-02 03:08:23 +0900

    Update GameManager.cs

--- project/KartRider/Manager/GameManager.cs
+++ project/KartRider/Manager/GameManager.cs
@@ -33,9 +33,21 @@ public class GameManager : MonoBehaviour
     float _carSpeed;//차량의 현재 속도
     float _speedLimit = 120f;//속도 제한
     float _racingDist = 0;//주행 거리
-  
+    float _touque = 900;//차량 가속도
+    
+    bool _isBreaking = false;//브레이크 여부
+    float _breakPower = 20000;//브레이크 파워
+
+    bool _isBooster = false;//부스터 여부
+
     public float CarSpeed { get { return _carSpeed; } set { _carSpeed = value; } }
     public float SpeedLimit { get { return _speedLimit; } set { _speedLimit = value; } }
     public float RacingDist { get { return _racingDist; } set { _racingDist = value; } }
+    public float Touque { get { return _touque; } set { _touque = value; } }
+
+    public bool IsBooster { get { return _isBooster; } set { _isBooster = value; } }
+
+    public bool IsBreaking { get { return _isBreaking; } set { _isBreaking = value; } }
+    public float BreakPower { get { return _breakPower; } set { _breakPower = value; } }
     #endregion
 }
