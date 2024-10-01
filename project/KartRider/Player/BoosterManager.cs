dd3f5c3826538a6df2bc79631ce4df5546e67a11
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-10-02 03:07:53 +0900

    Create BoosterManager.cs

---
+++ project/KartRider/Player/BoosterManager.cs
@@ -0,0 +1,48 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        GetKeyBooster();
+    }
+    /// <summary>
+    /// 기능 : 부스터 키 입력 받음
+    /// </summary>
+    async void GetKeyBooster()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            await BoosterOn();
+        }
+    }
+    /// <summary>
+    /// 기능 : 부스터 작동
+    /// 1) 최고 제한속도를 늘린다.
+    /// 2) 가속도를 늘린다.
+    /// 3) 3초후 브레이크를 걸어 속도를 줄인다.(부스터 끝)
+    /// 4) 최고 제한속도, 가속도 원래대로
+    /// 5) 속도 줄이는 효과를 1초간 브레이크 주는것으로 구현
+    /// TODO : 부스터 브레이크 파워,평소 브레이크 파워를 나눠야 함
+    /// </summary>
+    async UniTask BoosterOn()
+    {
+        GameManager.Instance.SpeedLimit = 150;
+        GameManager.Instance.Touque = 1500;
+   
+        await UniTask.Delay(3000);
+        
+        GameManager.Instance.SpeedLimit = 120;
+        GameManager.Instance.IsBooster = true;
+        GameManager.Instance.BreakPower = 500000;
+
+        await UniTask.Delay(1000);
+        GameManager.Instance.Touque = 900;
+        GameManager.Instance.BreakPower = 20000;
+        GameManager.Instance.IsBooster = false;
+    }
+}
