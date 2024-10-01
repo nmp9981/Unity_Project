9e4442ce8ea440891c9f62a3acf6e38f53096a12
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-10-02 03:08:39 +0900

    Update CarController.cs

--- project/KartRider/Player/CarController.cs
+++ project/KartRider/Player/CarController.cs
@@ -11,14 +11,9 @@ public class CarController : MonoBehaviour
     //질량 중심
     public GameObject centerOfMass;
 
-    public float Touque = 1200f;
     //최대 차량 회전 각도
-    public float steeringMaxAxis = 18f;
-    
-    //브레이크 여부
-    public bool isBreaking = false;
-    public float breakPower = 5000f;
-
+    public float steeringMaxAxis = 15f;
+  
     private void Awake()
     {
         carRigid = gameObject.GetComponent<Rigidbody>();
@@ -37,22 +32,33 @@ public class CarController : MonoBehaviour
     }
     /// <summary>
     /// 기능 : 바퀴 모터 돌리면서 자동차 이동
+    /// 가속 조건 : 제한 속도 이하, 위 키 누름, 브레이크 상태가 아님
     /// </summary>
     void MoveCar()
     {
         GameManager.Instance.CarSpeed = carRigid.velocity.magnitude*3.6f;
         carRigid.centerOfMass = centerOfMass.transform.localPosition;
         
-        if (GameManager.Instance.CarSpeed <= GameManager.Instance.SpeedLimit)
+        float verticalAmount = Input.GetAxis("Vertical");
+        if (GameManager.Instance.CarSpeed <= GameManager.Instance.SpeedLimit && verticalAmount!=0 && !GameManager.Instance.IsBreaking)
         {
             for (int i = 0; i < 4; i++)
             {
-                wheels[i].motorTorque = Touque * Input.GetAxis("Vertical");
+                wheels[i].motorTorque = GameManager.Instance.Touque * verticalAmount;
+            }
+        }
+        //제한 속도 초과
+        else
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                wheels[i].motorTorque = 0;
             }
         }
     }
     /// <summary>
     /// 기능 : 자동차 회전
+    /// 1) 사용자가 직접 회전 제어한다.
     /// </summary>
     void SteerCar()
     {
@@ -63,6 +69,7 @@ public class CarController : MonoBehaviour
     }
     /// <summary>
     /// 기능 : 자동차 회전 
+    /// 1) 바퀴가 직접 움직인다.
     /// </summary>
     void AnimateWheelMeshs()
     {
@@ -77,23 +84,24 @@ public class CarController : MonoBehaviour
     }
     /// <summary>
     /// 기능 : 브레이크
+    /// 조건 : 부스터 직후 or 왼쪽 shift키를 누름
     /// </summary>
     void Breaking()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift) || GameManager.Instance.IsBooster)
         {
-            isBreaking = true;
+            GameManager.Instance.IsBreaking = true;
         }
         else
         {
-            isBreaking = false;
+            GameManager.Instance.IsBreaking = false;
         }
 
-        if (isBreaking)
+        if (GameManager.Instance.IsBreaking)
         {
             for(int i=0;i<4; i++)
             {
-                wheels[i].brakeTorque = breakPower;
+                wheels[i].brakeTorque = GameManager.Instance.BreakPower;
             }
         }
         else
