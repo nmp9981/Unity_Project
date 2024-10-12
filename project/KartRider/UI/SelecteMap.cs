b7bff567968cc7e5392da04f766085b62c19e570
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-10-12 13:30:32 +0900

    Create SelecteMap.cs

---
+++ project/KartRider/UI/SelecteMap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SelecteMap : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)//마우스 클릭 이벤트 함수
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)//우클릭시
+        {
+            //맵 태그 아닌 경우 빈 문자열
+            string mapName = string.Empty;
+            if (gameObject.tag != "Map") mapName = string.Empty;
+            else mapName = gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text;
+            
+            GameManager.Instance.CurrentMap = mapName;
+        }
+    }
+}
