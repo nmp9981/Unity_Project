af6e92d063e819ba1156016c37dc9efc64c19300
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-10-20 00:48:51 +0900

    Create SoundManger.cs

---
+++ project/KartRider/Manager/SoundManger.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManger : MonoBehaviour
+{
+    [SerializeField] static GameObject _BGMManager;
+    [SerializeField] static GameObject _SFXManager;
+
+    static public SoundManger _sound;
+
+    [SerializeField] AudioClip[] _bgmClip;
+    [SerializeField] AudioClip[] _sfxClip;
+    static AudioSource _audioBgmSource;
+    static AudioSource _audioSfxSource;
+
+    static void Init()
+    {
+        if (_sound == null)
+        {
+            GameObject gm = GameObject.Find("SoundManager");
+            if (gm == null)
+            {
+                gm = new GameObject { name = "SoundManager" };
+
+                gm.AddComponent<SoundManger>();
+            }
+            DontDestroyOnLoad(gm);
+            _sound = gm.GetComponent<SoundManger>();
+
+            _BGMManager = GameObject.Find("BGMManager");
+            _SFXManager = GameObject.Find("SFXManager");
+            _audioBgmSource = _BGMManager.GetComponent<AudioSource>();
+            _audioSfxSource = _SFXManager.GetComponent<AudioSource>();
+        }
+    }
+    void Awake()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        _audioBgmSource.volume = GameManager.Instance.BGMVolume;
+        _audioSfxSource.volume = GameManager.Instance.SFXVolume;
+    }
+    public void PlayBGM(int musicNum)
+    {
+        _audioBgmSource.clip = _bgmClip[musicNum];
+        _audioBgmSource.volume = GameManager.Instance.BGMVolume;//상시 조절되게 바깥으로 뺌
+        _audioBgmSource.loop = true;
+        _audioBgmSource.Play();
+    }
+    public void StopBGM(int musicNum)
+    {
+        _audioBgmSource.clip = _bgmClip[musicNum];
+        if (_audioBgmSource.isPlaying)
+        {
+            _audioBgmSource.Stop();
+        }
+    }
+    public void PlaySfx(int soundNum)
+    {
+        _audioSfxSource.volume = GameManager.Instance.SFXVolume;//변수 값으로 조절
+        _audioSfxSource.playOnAwake = true;
+        _audioSfxSource.loop = false;
+        _audioSfxSource.PlayOneShot(_sfxClip[soundNum]);//한개만 적용
+    }
+}
