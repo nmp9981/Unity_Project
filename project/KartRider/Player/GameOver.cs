a26668295a2a9e6e4672f8a9c5eef8afee9119fe
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-10-23 01:53:28 +0900

    Create GameOver.cs

---
+++ project/KartRider/Player/GameOver.cs
@@ -0,0 +1,42 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    void Update()
+    {
+        RacingFinish();
+    }
+    /// <summary>
+    /// 기능 : 레이싱 종료 조건 체크
+    /// 조건1 : 인게임 씬
+    /// 조건2 : 남은 시간이 0이하 or 주행중인 상태가 아닐 때
+    /// </summary>
+    bool CheckFinish()
+    {
+        if (!SceneManager.GetActiveScene().name.Contains("KartGame"))
+        {
+            return false;
+        }
+        if (GameManager.Instance.CurrentTime <= 0 || 
+            GameManager.Instance.CurrentLap >= GameManager.Instance.mapLapList[GameManager.Instance.CurrentMapIndex])
+        {
+            return true;
+        }
+        return false;
+    }
+    /// <summary>
+    /// 주행 종료 알림
+    /// </summary>
+    async UniTask RacingFinish()
+    {
+        if (CheckFinish())
+        {
+            await UniTask.Delay(5000);
+            GameManager.MoveToMainScene();
+        }
+    }
+}
