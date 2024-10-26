2df1ace72b389b9cc1d3d402f47104b1b5b794e6
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-10-26 11:58:22 +0900

    Update MenuUIManager.cs

--- project/KartRider/UI/MenuUIManager.cs
+++ project/KartRider/UI/MenuUIManager.cs
@@ -11,13 +11,14 @@ public class MenuUIManager : MonoBehaviour
 {
     //선택한 맵 이름
     public static TextMeshProUGUI selectMapText;
-
-    //시작 버튼
-    Button startButton;
+    //맵 페이지 오브젝트 리스트
+    [SerializeField]
+    List<GameObject> mapPageList = new List<GameObject>();
 
     private void Awake()
     {
         SettingMainUI();
+        MenuButtonBinding();
     }
     private void Start()
     {
@@ -26,15 +27,28 @@ public class MenuUIManager : MonoBehaviour
     /// <summary>
     /// 기능 : 메인 UI 세팅
     /// 1) 맵 선택 칸
-    /// 2) 시작 버튼 바인딩
+    /// 2) 맵 페이지 이동 버튼 오브젝트 등록
     /// </summary>
     void SettingMainUI()
     {
         selectMapText = GameObject.Find("MapTextBackGround").GetComponentInChildren<TextMeshProUGUI>();
         selectMapText.text = string.Empty;
-
-        startButton = GameObject.Find("RacingStart").GetComponentInChildren<Button>();
+    }
+    /// <summary>
+    /// 기능 : 메뉴 버튼 바인딩
+    /// 1) 시작 버튼 바인딩
+    /// 2) 맵 페이지 이동 버튼 바인딩
+    /// </summary>
+    void MenuButtonBinding()
+    {
+        var startButton = GameObject.Find("RacingStart").GetComponentInChildren<Button>();
         startButton.onClick.AddListener(() => GoRacingStart(GameManager.Instance.CurrentMap));
+
+        var prevMapPageButton = GameObject.Find("PrevPage").GetComponent<Button>();
+        prevMapPageButton.onClick.AddListener(() => MovePrevMapPage());
+
+        var nextMapPageButton = GameObject.Find("NextPage").GetComponent<Button>();
+        nextMapPageButton.onClick.AddListener(() => MoveNextMapPage());
     }
     /// <summary>
     /// 기능 : 고른 맵으로 이동
@@ -54,7 +68,28 @@ public class MenuUIManager : MonoBehaviour
             int mapNumber = GameManager.mapDictoinaty[mapName];
             GameManager.MoveStartPositionAndDataInit(mapNumber);
             int musicNum = 2*mapNumber+(Random.Range(0, 10) % 2)+1;
+            if (musicNum > 6) musicNum = 6;
             SoundManger._sound.PlayBGM(musicNum);
         }
+    }
+    /// <summary>
+    /// 기능 : 이전 맵 페이지로 이동
+    /// </summary>
+    public void MovePrevMapPage()
+    {
+        mapPageList[GameManager.Instance.CurrentMapPageIndex].SetActive(false);
+        GameManager.Instance.CurrentMapPageIndex-=1;
+        if (GameManager.Instance.CurrentMapPageIndex <= 0) GameManager.Instance.CurrentMapPageIndex = 0;
+        mapPageList[GameManager.Instance.CurrentMapPageIndex].SetActive(true);
+    }
+    /// <summary>
+    /// 기능 ; 다음 맵 페이지로 이동
+    /// </summary>
+    public void MoveNextMapPage()
+    {
+        mapPageList[GameManager.Instance.CurrentMapPageIndex].SetActive(false);
+        GameManager.Instance.CurrentMapPageIndex += 1;
+        if (GameManager.Instance.CurrentMapPageIndex > 1) GameManager.Instance.CurrentMapPageIndex = 1;
+        mapPageList[GameManager.Instance.CurrentMapPageIndex].SetActive(true);
     }
 }
