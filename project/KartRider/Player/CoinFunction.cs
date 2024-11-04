d37249195993aa992873e938cf647ebef5ccf044
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-11-04 21:48:27 +0900

    Create CoinFunction.cs

---
+++ project/KartRider/Player/CoinFunction.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinFunction : MonoBehaviour
+{
+    const float rotationAngle = 30;
+    void Update()
+    {
+        RotateCoin();
+    }
+    //코인 회전
+    void RotateCoin()
+    {
+        gameObject.transform.parent.transform.Rotate(rotationAngle*Time.deltaTime,0,0);
+    }
+    //충돌 시 삭제
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            int getLucci = Random.Range(50, 100);
+            GameManager.Instance.PlayerLucci += getLucci;
+            gameObject.SetActive(false);
+            Invoke("RegenerateCoin",10f);
+        }
+    }
+    // 10초 후 코인 재생성
+    void RegenerateCoin()
+    {
+        gameObject.SetActive(true);
+    }
+}
