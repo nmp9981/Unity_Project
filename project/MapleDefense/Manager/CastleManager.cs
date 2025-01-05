ac628340ac1a900c4ece34f20ad3b53d06138d2c
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-01-05 12:05:10 +0900

    Update CastleManager.cs

--- project/MapleDefense/Manager/CastleManager.cs
+++ project/MapleDefense/Manager/CastleManager.cs
@@ -6,6 +6,8 @@ public class CastleManager : MonoBehaviour
 {
     [SerializeField]
     GameObject castleUI;
+    [SerializeField]
+    GameObject upgradeUI;
 
     TextMeshProUGUI expRateText;
     TextMeshProUGUI hpRateText;
@@ -19,6 +21,7 @@ public class CastleManager : MonoBehaviour
     {
         TextBinding();
         ImageBinding();
+        ButtonBinding();
     }
     private void Start()
     {
@@ -73,7 +76,31 @@ public class CastleManager : MonoBehaviour
         }
     }
     /// <summary>
-    /// 초기 세팅
+    /// 기능 : UI 버튼 바인딩
+    /// </summary>
+    void ButtonBinding()
+    {
+        foreach (Button btn in castleUI.GetComponentsInChildren<Button>(true))
+        {
+            string btnName = btn.gameObject.name;
+            switch (btnName)
+            {
+                case "UnitSet":
+                    btn.onClick.AddListener(OpenUnitUpgradeButton);
+                    break;
+                case "Setting":
+                    btn.onClick.AddListener(OpenSettingButton);
+                    break;
+                case "ReturnMenu":
+                    btn.onClick.AddListener(ReturnMainMenu);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    /// <summary>
+    /// 기능 : 초기 세팅
     /// </summary>
     void InitSettingCastleValue()
     {
@@ -87,5 +114,29 @@ public class CastleManager : MonoBehaviour
 
         hpBarImage.fillAmount = 1;
         expBarImage.fillAmount = 0;
+    }
+    /// <summary>
+    /// 기능 : 메인메뉴로 돌아가기
+    /// </summary>
+    void ReturnMainMenu()
+    {
+
+    }
+    /// <summary>
+    /// 기능 : 세팅창 열기
+    /// </summary>
+    void OpenSettingButton()
+    {
+
+    }
+    /// <summary>
+    /// 기능 : 유닛 업그레이드 창 열기
+    /// </summary>
+    void OpenUnitUpgradeButton()
+    {
+        if (!upgradeUI.activeSelf)
+        {
+            upgradeUI.SetActive(true);
+        }
     }
 }
