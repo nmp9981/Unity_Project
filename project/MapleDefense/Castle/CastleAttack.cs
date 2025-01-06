2f39a6bdb3734e0e55017a288bbab8f0272ec8b5
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-01-07 02:45:19 +0900

    Update CastleAttack.cs

--- project/MapleDefense/Castle/CastleAttack.cs
+++ project/MapleDefense/Castle/CastleAttack.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CastleAttack : MonoBehaviour
 {
     public GameObject throwObjectPrefab;
+    GameObject finalTarget;
     float maxDist = 99999999;
     void Start()
     {
-        SearchNearTarget();
+        InvokeRepeating("SearchNearTarget", 0.5f, 1f);
     }
 
+    void CreateThrowObject()
+    {
+        GameObject throwObject = Instantiate(throwObjectPrefab);
+        throwObject.transform.position = this.gameObject.transform.position;
+        throwObject.GetComponent<ThrowObject>().target = finalTarget;
+    }
     /// <summary>
     /// 가장 가까운 타겟 찾기
     /// </summary>
     void SearchNearTarget()
     {
-        GameObject finalTarget = null;
+        finalTarget = null;
         //대상 없음
         if (GameManager.Instance.ActiveUnitList.Count == 0)
         {
@@ -25,15 +33,17 @@ public class CastleAttack : MonoBehaviour
         foreach (var gm in GameManager.Instance.ActiveUnitList)
         {
             float dist = (gm.gameObject.transform.position - this.gameObject.transform.position).sqrMagnitude;
-            //더 가까운 거리
-            if (dist<curDist)
+            //더 가까운 거리 (사거리 내)
+            if (dist < curDist && dist < 200)
             {
                 curDist = dist;
                 finalTarget = gm;
             }
         }
-
-        GameObject throwObject = Instantiate(throwObjectPrefab);
-        throwObject.GetComponent<ThrowObject>().target = finalTarget;
+        //타겟 존재시 총알생성
+        if(finalTarget != null)
+        {
+            CreateThrowObject();
+        }
     }
 }
