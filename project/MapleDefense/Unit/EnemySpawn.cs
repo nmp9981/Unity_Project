8f4193c2e3cc090d29eec09837a36b68435755bf
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-01-11 12:32:00 +0900

    Update EnemySpawn.cs

--- project/MapleDefense/Unit/EnemySpawn.cs
+++ project/MapleDefense/Unit/EnemySpawn.cs
@@ -21,11 +21,26 @@ public class EnemySpawn : MonoBehaviour
         while (true)
         {
             //어떤 적을 소환할지
-            int mobNum = Random.Range(0, 2);
+            int mobNum = SetSpawnEnemy();
             GameObject mob = objectFulling.MakeObj(mobNum);
             mob.transform.position = spawnPoint.position;
+            //활성화 몬스터에 추가
+            GameManager.Instance.ActiveUnitList.Add(mob);
 
             await UniTask.Delay(GameManager.Instance.SpawnTime);
         }
+    }
+    int SetSpawnEnemy()
+    {
+        int mobNum = 0;
+        if (GameManager.Instance.CurrentStage <= 1)
+        {
+            mobNum = Random.Range(0, 2);
+        }
+        else
+        {
+            mobNum = Random.Range(3, 5);
+        }
+        return mobNum;
     }
 }
