5988470299c04475daae03ffa98053ab0b59e171
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-01-25 12:58:20 +0900

    Update EnemySpawn.cs

--- project/MapleDefense/Unit/EnemySpawn.cs
+++ project/MapleDefense/Unit/EnemySpawn.cs
@@ -20,14 +20,18 @@ public class EnemySpawn : MonoBehaviour
     {
         while (true)
         {
-            //어떤 적을 소환할지
-            int mobNum = SetSpawnEnemy();
-            GameObject mob = objectFulling.MakeObj(mobNum);
-            mob.transform.position = spawnPoint.position;
-            //활성화 몬스터에 추가
-            GameManager.Instance.ActiveUnitList.Add(mob);
+            //업그레이드 UI창 비활성화 일때
+            if (!GameManager.Instance.IsOpenUpgradeUI)
+            {
+                //어떤 적을 소환할지
+                int mobNum = SetSpawnEnemy();
+                GameObject mob = objectFulling.MakeMonsterObj(mobNum);
+                mob.transform.position = spawnPoint.position;
+                //활성화 몬스터에 추가
+                GameManager.Instance.ActiveUnitList.Add(mob);
 
-            await UniTask.Delay(GameManager.Instance.SpawnTime);
+                await UniTask.Delay(GameManager.Instance.SpawnTime);
+            }
         }
     }
     /// <summary>
@@ -36,7 +40,7 @@ public class EnemySpawn : MonoBehaviour
     /// <returns></returns>
     int SetSpawnEnemy()
     {
-        int mobNum = Random.Range(GameManager.Instance.CurrentStage, GameManager.Instance.CurrentStage + 2);
+        int mobNum = Random.Range(GameManager.Instance.CurrentStage-1, GameManager.Instance.CurrentStage + 1);
         return mobNum;
     }
 }
