9dc11581b219bfe097c2b0b7de24c211b0856cec
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-01-31 23:12:59 +0900

    Create EnemyThrowObjectClass.cs

---
+++ project/MapleDefense/Unit/EnemyThrowObjectClass.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyThrowObjectClass : MonoBehaviour
+{
+    /// <summary>
+    /// 몬스터 공격력
+    /// </summary>
+    public int monsterBasicAttack;
+
+    [SerializeField]
+    float moveSpeed;
+
+    Transform targetCastle;
+    CastleManager castleManager;
+    private void Awake()
+    {
+        GameObject castleObj = GameObject.Find("Castle");
+        castleManager = castleObj.GetComponent<CastleManager>();
+        targetCastle = castleObj.transform;
+    }
+
+    private void Update()
+    {
+        MoveToCastle();
+    }
+    void MoveToCastle()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetCastle.position, Time.deltaTime * moveSpeed);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Contains("CastleEnter"))
+        {
+            //피격 효과음
+            SoundManager._sound.PlaySfx((int)SFXSound.CastleHit);
+            DecreaseCastleHP(monsterBasicAttack);
+        }
+    }
+    /// <summary>
+    /// HP 감소
+    /// </summary>
+    /// <param name="monsterAttack">몬스터 공격력</param>
+    void DecreaseCastleHP(int monsterAttack)
+    {
+        GameManager.Instance.CurrentCastleHP -= (monsterAttack * 3 / 2);
+        castleManager.ShowCastleHP();
+        Destroy(this.gameObject);
+    }
+}
