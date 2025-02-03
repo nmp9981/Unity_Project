b74f5927c17345d2ad41b3f6e4ac9050f69dbefd
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-02-04 01:21:00 +0900

    Update SupporterUnit.cs

--- project/MapleDefense/Unit/SupporterUnit.cs
+++ project/MapleDefense/Unit/SupporterUnit.cs
@@ -1,3 +1,6 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +12,12 @@ public class SupporterUnit : MonoBehaviour
     [SerializeField]
     float rayInspectDist;//ray 인식 거리
 
+    //투사체 날리기
+    [SerializeField]
+    GameObject supporterBullet;
+    [SerializeField]
+    Transform startShootPos;
+
     /// <summary>
     /// 소환수 HP
     /// </summary>
@@ -16,29 +25,65 @@ public class SupporterUnit : MonoBehaviour
     float fullSupportHP;
     [SerializeField]
     float currentSupportHP;
+
     [SerializeField]
-    int supportAttack;
+    public int supportAttack;
+    [SerializeField]
+    float supportAttackSpeed = 0.5f;
     [SerializeField]
     Image hpBarBack;
     [SerializeField]
     Image hpBar;
 
+    Animator anim;
+    public bool isFighting;//전투중인가?
 
+    float noHitDamageCurrentTime = 0f;
+    float attackCurrentTime = 0f;
 
     const float maxInspectDist = 15;
+    const float notHitDamageTime = 1;
+
+    private void Awake()
+    {
+        InitAnimation();
+    }
 
     private void OnEnable()
     {
+        isFighting = false;
         currentSupportHP = fullSupportHP;
         hpBar.fillAmount = 1f;
+
+        anim.Play("Move");
+        StartCoroutine(SupporterAttack());
     }
     
     void Update()
     {
         MoveSupportUnit();
         InspectEnemy();
-        SupporterAttack();
+        TimeFlow();
     }
+    /// <summary>
+    /// 애니메이션 초기화
+    /// </summary>
+    void InitAnimation()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        anim.SetBool("isDie", false);
+        anim.SetBool("isAttack", false);
+        anim.SetBool("isHit", false);
+    }
+    /// <summary>
+    /// 시간 흐름
+    /// </summary>
+    void TimeFlow()
+    {
+        noHitDamageCurrentTime += Time.deltaTime;
+        attackCurrentTime += Time.deltaTime;
+    }
+
     /// <summary>
     /// 소환수 이동
     /// 전투중이, UI업그레이드 활성화가 아닐때만 이동
@@ -46,11 +91,18 @@ public class SupporterUnit : MonoBehaviour
     /// </summary>
     void MoveSupportUnit()
     {
-        if (!GameManager.Instance.IsFighting && !GameManager.Instance.IsOpenUpgradeUI)
+        if (!isFighting && !GameManager.Instance.IsOpenUpgradeUI)
         {
             transform.position += Vector3.right * Time.deltaTime * unitSpeed;
             HPBarMove();
         }
+        else
+        {
+            transform.position += Vector3.right * Time.deltaTime * 0;
+        }
+
+        //소환수 수명 종료
+        UnitDestroy();
     }
     /// <summary>
     /// 기능 : HPBar 이동
@@ -73,42 +125,120 @@ public class SupporterUnit : MonoBehaviour
             //적일 경우
             if (rayHitObj.collider.gameObject.CompareTag("Enemy"))
             {
-                //Debug.Log("몬스터 인식");
-                GameManager.Instance.IsFighting = true;
+                isFighting = true;
+            }
+            else
+            {
+                isFighting = false;
             }
         }
         else
         {
-            GameManager.Instance.IsFighting = false;
+            isFighting = false;
+        }
+    }
+   
+    /// <summary>
+    /// 기능 : 적 공격
+    /// </summary>
+    IEnumerator SupporterAttack()
+    {
+        //전투중이 아님
+        if (!isFighting)
+        {
+            yield break;
+        }
+
+        yield return new WaitForSeconds(supportAttackSpeed);
+       
+        while (true)
+        {
+            //공격 쿨타임이 됨
+            anim.SetBool("isAttack", true);
+            
+            //투사체 날리기(anim 실행시간)
+            yield return new WaitForSeconds(0.1f);
+            float readyTime = anim.GetCurrentAnimatorStateInfo(0).length;
+            yield return new WaitForSeconds(readyTime);
+            SupporterAttackThrowObject();
+
+            yield return new WaitForSeconds(supportAttackSpeed * 0.1f);
+            anim.SetBool("isAttack", false);
+            yield return new WaitForSeconds(supportAttackSpeed);
         }
     }
     /// <summary>
-    /// 기능 : 공격
+    /// 투사체 날리기
     /// </summary>
-    void SupporterAttack()
+    void SupporterAttackThrowObject()
     {
-        if (!GameManager.Instance.IsFighting)
+        //투사체가 없음
+        if (supporterBullet == null)
         {
             return;
         }
+        //투사체 발사 및 공격력 설정
+        GameObject monsterBulletObj = Instantiate(supporterBullet);
+        monsterBulletObj.transform.position = startShootPos.position;
+        monsterBulletObj.GetComponent<SupporterThrowObjectClass>().supporterBasicAttack = supportAttack;
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
+        //피격
         if (collision.tag.Contains("Enemy"))
         {
-            //HP감소
             EnemyUnit enemyUnit = collision.gameObject.GetComponent<EnemyUnit>();
-
-            enemyUnit.HP -= supportAttack;
-            currentSupportHP -= enemyUnit.Attack;
-            float hpRate = (float)currentSupportHP / fullSupportHP;
-            hpBar.fillAmount = hpRate;
+            //적 HP감소
+            if (isFighting)
+            {
+                DecreaseEnemyHP(enemyUnit);
+            }
+            
+            //소환수 HP감소
+            DecreaseSupportHP(enemyUnit);
 
             //사망처리
             if (currentSupportHP <= 0)
             {
                 gameObject.SetActive(false);
             }
+        }
+    }
+
+    
+    /// <summary>
+    /// 적 HP감소
+    /// </summary>
+    void DecreaseEnemyHP(EnemyUnit enemyUnit)
+    {
+        if(attackCurrentTime >= supportAttackSpeed)
+        {
+            enemyUnit.DecreaseEnemyUnitHP(supportAttack);
+            attackCurrentTime = 0;
+        }
+    }
+    /// <summary>
+    /// 소환수 HP감소
+    /// </summary>
+    void DecreaseSupportHP(EnemyUnit enemyUnit)
+    {
+        if(noHitDamageCurrentTime >= notHitDamageTime)
+        {
+            currentSupportHP -= enemyUnit.Attack;
+            float hpRate = (float)currentSupportHP / fullSupportHP;
+            hpBar.fillAmount = hpRate;
+            noHitDamageCurrentTime = 0;
+        }
+    }
+    /// <summary>
+    /// 적 성에 닿으면 사라짐
+    /// </summary>
+    void UnitDestroy()
+    {
+        if (gameObject.transform.position.x > 10)
+        {
+            gameObject.SetActive(false);
         }
     }
 }
