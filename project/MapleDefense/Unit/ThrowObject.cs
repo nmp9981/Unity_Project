8864675502e11bfc7f07749a7ea0f5ebec34f949
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-02-04 23:57:05 +0900

    Create ThrowObject.cs

---
+++ project/MapleDefense/Unit/ThrowObject.cs
@@ -0,0 +1,48 @@
+using Cysharp.Threading.Tasks.Triggers;
+using UnityEngine;
+
+public class ThrowObject : MonoBehaviour
+{
+    public int Attack;
+    public float speed;
+    public GameObject target;
+
+    //어느 무기에서 발사했는가?
+    public CastleAttack fromWeapon;
+
+    Vector3 dir;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Contains("Enemy"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void Update()
+    {
+        ThrowMove();
+    }
+    /// <summary>
+    /// 기능 : 타겟 세팅
+    /// </summary>
+    public void TargetSetting(GameObject targ)
+    {
+        target = targ;
+        transform.LookAt(target.transform.position);
+        //y축 값을 0으로 (y값만 변경)
+        gameObject.transform.eulerAngles = new Vector3(gameObject.transform.rotation.x,0,
+         gameObject.transform.rotation.z);
+        dir = (target.transform.position - gameObject.transform.position).normalized;
+    }
+    /// <summary>
+    /// 기능 : 투사체 이동
+    /// </summary>
+    void ThrowMove()
+    {
+        gameObject.transform.position += dir * Time.deltaTime * speed;
+        if (gameObject.transform.position.x > 10)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
