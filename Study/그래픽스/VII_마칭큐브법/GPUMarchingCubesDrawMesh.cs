dcbcd9d57bb8a920c7f039ad709fb6f4da1f3f97
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-03-14 03:00:48 +0900

    Update GPUMarchingCubesDrawMesh.cs

--- Study/그래픽스/VII_마칭큐브법/GPUMarchingCubesDrawMesh.cs
+++ Study/그래픽스/VII_마칭큐브법/GPUMarchingCubesDrawMesh.cs
@@ -30,7 +30,18 @@ public class GPUMarchingCubesDrawMesh : MonoBehaviour
     MarchingCubesDefines mcDefines = null;//마칭큐브용 정수 배열
     #endregion
 
-    //초기화
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Update()
+    {
+        //즉석에서 그려지지 않고 렌더링 처리에 우선 등록함
+        RenderMesh();
+    }
+
+    //초기화 -> 쉐이더에서는 리터럴값이 4096개가 한계이므로 여기서 초기화
     void Initialize()
     {
         //총 정점 필요 개수 (32^3)
@@ -46,22 +57,29 @@ public class GPUMarchingCubesDrawMesh : MonoBehaviour
     //메시 제작
     void CreateMesh()
     {
-        //의
+        //Mesh의 정점 개수는 65535가 상한, Mesh분해
         int vertNum = 65535;
-        int meshNum = Mathf.CeilToInt((float)vertexMax / vertNum); 
+        //분해 하는 메시의 수
+        int meshNum = Mathf.CeilToInt((float)vertexMax / vertNum);
         Debug.Log("meshNum " + meshNum);
+        
+        //배열 할당
         meshs = new Mesh[meshNum];
         materials = new Material[meshNum];
-        //의
+        
+        //메시 Bounds계산
+        //현재 위치 + 격자 크기만큼
         Bounds bounds = new Bounds(
          transform.position,
          new Vector3(segmentNum, segmentNum, segmentNum) * renderScale
          );
+
         int id = 0;
         for (int i = 0; i < meshNum; i++)
         {
-            //
+            //정점 작성
             Vector3[] vertices = new Vector3[vertNum];
+            //정점 번호 저장
             int[] indices = new int[vertNum];
             for (int j = 0; j < vertNum; j++)
             {
@@ -71,13 +89,38 @@ public class GPUMarchingCubesDrawMesh : MonoBehaviour
                 indices[j] = j;
                 id++;
             }
-            //
+            //Mesh 작성
             meshs[i] = new Mesh();
             meshs[i].vertices = vertices;
-            // d 
+            //GeometryShader로 폴리곤을 만드므로 point가 좋다
             meshs[i].SetIndices(indices, MeshTopology.Points, 0);
             meshs[i].bounds = bounds;
+
             materials[i] = new Material(mat);
+        }
+    }
+    //렌더링 부분
+    void RenderMesh()
+    {
+        Vector3 halfSize = new Vector3(segmentNum, segmentNum, segmentNum) * renderScale * 0.5f;
+        //이동, 회전, 크기 행렬 정의
+        Matrix4x4 trs = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
+
+        //computeBuffer들을 쉐이더에 넘김 넘기기
+        for (int i = 0; i < meshs.Length; i++)
+        {
+            materials[i].SetPass(0);
+            materials[i].SetInt("_SegmentNum", segmentNum);
+            materials[i].SetFloat("_Scale", renderScale);
+            materials[i].SetFloat("_Threashold", threashold);
+            materials[i].SetFloat("_Metallic", metallic);
+            materials[i].SetFloat("_Glossiness", glossiness);
+            materials[i].SetFloat("_EmissionIntensity", EmissionIntensity);
+            materials[i].SetVector("_HalfSize", halfSize);
+            materials[i].SetColor("_DiffuseColor", DiffuseColor);
+            materials[i].SetColor("_EmissionColor", EmissionColor);
+            materials[i].SetMatrix("_Matrix", trs);
+            Graphics.DrawMesh(meshs[i], Matrix4x4.identity, materials[i], 0);
         }
     }
 }
