fb1e7e4394afee0610d707ca5878483f52e479ab
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-03-24 00:07:34 +0900

    Create PlayerInfo.cs

---
+++ project/SkillTest/Common/PlayerInfo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    [SerializeField]
+    public static long maxAttackDamage = 1350;
+    public static long workmanship = 60;
+
+    public static int criticalRate = 40;
+    public static long criticalDamageRate = 2;
+}
