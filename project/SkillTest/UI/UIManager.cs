d42e61432c207df60f0cefa838d382ee6e72c9b3
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-03-23 14:45:36 +0900

    Create UIManager.cs

---
+++ project/SkillTest/UI/UIManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI accumulateDamageText;
+
+    public List<Sprite> damageImage = new List<Sprite>();
+    public List<Sprite> criticalDamageImage = new List<Sprite>();
+    //누적 데미지
+    long accumulateDamage = 0;
+    
+    public void ShowDamage(long damage)
+    {
+        accumulateDamage += damage;
+        accumulateDamageText.text = accumulateDamage.ToString();
+    }
+}
