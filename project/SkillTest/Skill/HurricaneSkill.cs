4766850d19c39f63f6ed5066170a1d1502c215ac
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-03-26 02:04:22 +0900

    Update HurricaneSkill.cs

--- project/SkillTest/Skill/HurricaneSkill.cs
+++ project/SkillTest/Skill/HurricaneSkill.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HurricaneSkill : MonoBehaviour
@@ -7,12 +9,25 @@ public class HurricaneSkill : MonoBehaviour
     [SerializeField]
     GameObject player;
     [SerializeField]
-    GameObject target;
-    [SerializeField]
     GameObject dirToPlayer;
+
+    Vector3 targetPosition;
+    List<Vector3> targetList = new List<Vector3>();
 
     const float angleRange = 45;
     const float radToDeg = 57.2958f;
+
+    private void Awake()
+    {
+        GameObject monsterSet = GameObject.Find("MonsterSet");
+        foreach (Transform monster in monsterSet.GetComponentsInChildren<Transform>()) 
+        {
+            if(monster.gameObject.tag == "Monster")
+            {
+                targetList.Add(monster.position);
+            }
+        }
+    }
 
     /// <summary>
     /// 폭풍의 시 Flow
@@ -26,12 +41,15 @@ public class HurricaneSkill : MonoBehaviour
         Projective projective = arrow.GetComponent<Projective>();
         arrow.transform.position = player.transform.position;
 
+        //타겟 선정
+        targetPosition = FindNearestTarget();
+
         //범위내에 드는가?
         bool isRange = IsRangeViewAngle();
         //플레이어 방향 벡터
         Vector3 playerDir = (dirToPlayer.transform.position - player.transform.position).normalized;
 
-        projective.InitArrowInfo(target.transform.position, playerDir,isRange,0,100);
+        projective.InitArrowInfo(targetPosition, playerDir,isRange,0,100);
     }
 
     /// <summary>
@@ -43,7 +61,7 @@ public class HurricaneSkill : MonoBehaviour
         //플레이어 방향 벡터
         Vector3 playerDir = (dirToPlayer.transform.position- player.transform.position).normalized;
         //타겟까지의 방향
-        Vector3 targetDir = (target.transform.position- player.transform.position).normalized;
+        Vector3 targetDir = (targetPosition - player.transform.position).normalized;
        
         //각도
         float angle = Vector3.Dot(playerDir, targetDir);
@@ -55,5 +73,24 @@ public class HurricaneSkill : MonoBehaviour
         }
         //범위내에 들지 않음
         return false;
+    }
+    /// <summary>
+    /// 타겟 찾기
+    /// </summary>
+    /// <returns></returns>
+    Vector3 FindNearestTarget()
+    {
+        Vector3 targetPos = Vector3.zero;
+        float dist = int.MaxValue;
+        foreach(var target in targetList)
+        {
+            float curDist = Vector3.Distance(player.transform.position, target);
+            if(curDist < dist)
+            {
+                dist = curDist;
+                targetPos = target;
+            }
+        }
+        return targetPos;
     }
 }
