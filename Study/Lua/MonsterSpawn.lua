140b74055100146e2ead999c3eb220f8d8e5eaee
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-04-04 01:47:00 +0900

    Update MonsterSpawn.lua

--- Study/Lua/MonsterSpawn.lua
+++ Study/Lua/MonsterSpawn.lua
@@ -7,11 +7,16 @@ for i=1,self.monsterMaxCount,1 do
 	--소환할 몬스터 번호
 	local monsterNum = math.random(monsterCountInMap)
 	--소환 몬스터 아이디
-	local id = _DataService:GetCell("MonsterDataBase",monsterNum,2)
+	local id = _DataService:GetCell("MonsterDataBase",monsterNum,1)
 	--소환할 몬스터 이름
-	local name = _DataService:GetCell("MonsterDataBase",monsterNum,1)
+	local name = _DataService:GetCell("MonsterDataBase",monsterNum,2)
 	--소환 위치
-	local Position = Vector3(math.random(-3,3),0,0)
+	local Position = Vector3(math.random(-4,4),0,0)
 	--소환
-	_SpawnService:SpawnByModelId(id, name,Position,parent)
+	local mob = _SpawnService:SpawnByModelId(id, name,Position,parent)
+	--몬스터 정보
+	mob.Monster.Lv = tonumber(_DataService:GetCell("MonsterDataBase",monsterNum,3))
+	mob.Monster.MaxHp = tonumber(_DataService:GetCell("MonsterDataBase",monsterNum,4))
+	mob.Monster.Hp = mob.Monster.MaxHp
+	mob.Monster.Exp = tonumber(_DataService:GetCell("MonsterDataBase",monsterNum,5))
 end
