1f69523337acdc787e9e12f8a46804348696d0f9
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-11-19 03:04:21 +0900

    Update 물리루프_렌더루프_시차_시각화.cs

--- Study/Physics/3rdMonth/물리루프_렌더루프_시차_시각화.cs
+++ Study/Physics/3rdMonth/물리루프_렌더루프_시차_시각화.cs
@@ -6,3 +6,19 @@ void FixedUpdate() {
 void Update() {
     Debug.DrawLine(transform.position, lastFixedPos, Color.red);
 }
+
+[프레임 시작]
+    Input 업데이트
+    Update()		    (1)
+    LateUpdate()	    (2)
+
+    while(accumulatedTime >= fixedDeltaTime)
+    {
+        FixedUpdate()   (3)
+        Physics Step    (4)
+        Collision       (5)
+        accumulatedTime -= fixedDeltaTime
+    }
+
+    Rendering		    (6)
+[프레임 끝]
