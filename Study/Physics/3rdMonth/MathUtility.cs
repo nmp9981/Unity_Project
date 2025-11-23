363fc3aac75ce0eedf4446d2f159f9b1bb277d34
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-11-23 16:56:54 +0900

    Update MathUtility.cs

--- Study/Physics/3rdMonth/MathUtility.cs
+++ Study/Physics/3rdMonth/MathUtility.cs
@@ -1,11 +1,32 @@
 public static class MathUtility
 {
     /// <summary>
+    /// 두 수중 작은 값 반환
+    /// </summary>
+    /// <param name="a">실수</param>
+    /// <param name="b">실수</param>
+    /// <returns>Min값</returns>
+    public static float Min(float a, float b)
+    {
+        return a < b ? a : b;
+    }
+    /// <summary>
+    /// 두 수중 큰 값 반환
+    /// </summary>
+    /// <param name="a">실수</param>
+    /// <param name="b">실수</param>
+    /// <returns>Min값</returns>
+    public static float Max(float a, float b)
+    {
+        return a > b ? a : b;
+    }
+
+    /// <summary>
     /// 가장 가까운 값 찾기
     /// </summary>
     /// <param name="standardValue">기준 값</param>
-    /// <param name="minValue"></param>
-    /// <param name="maxValue"></param>
+    /// <param name="minValue">범위 최솟값</param>
+    /// <param name="maxValue">범위 최댓값</param>
     /// <returns></returns>
     public static float ClampValue(float standardValue, float minValue, float maxValue)
     {
