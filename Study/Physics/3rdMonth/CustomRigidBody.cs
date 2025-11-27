68b95ed25e9a3bcfca203b57dc08dce267514066
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-11-28 02:59:30 +0900

    Update CustomRigidBody.cs

--- Study/Physics/3rdMonth/CustomRigidBody.cs
+++ Study/Physics/3rdMonth/CustomRigidBody.cs
@@ -8,8 +8,8 @@ public struct RigidbodyState
 public class CustomRigidBody : MonoBehaviour
 {
     [Header("Physical Properties")]
-    public Vec3 velocity;//속도
-    public Vec3 acceleration;//가속도
+    public Vec3 velocity = new Vec3(0,0,0);//속도
+    public Vec3 acceleration = new Vec3(0, 0, 0);//가속도
     public Mass mass = new Mass();//질량
     public Vec3 gravity3D = new Vec3(0, -9.81f,0);
 
@@ -34,6 +34,26 @@ public class CustomRigidBody : MonoBehaviour
 
     //결과 값
     public Vec3 deltaPosition = VectorMathUtils.ZeroVector3D();
+
+
+    private void Awake()
+    {
+        InitializePosition();
+    }
+
+    /// <summary>
+    /// 초기 위치 설정
+    /// </summary>
+    void InitializePosition()
+    {
+        Vec3 startPos = new Vec3(transform.position.x, transform.position.y, transform.position.z);
+        currentState.position = startPos;
+        previousState.position = startPos;
+
+        deltaPosition = VectorMathUtils.ZeroVector3D();
+        velocity = VectorMathUtils.ZeroVector3D();
+        acceleration = VectorMathUtils.ZeroVector3D();
+    }
 
     //Physics Step → Collision → Resolve → Commit → Render Step(Interpolation)
     public void PhysicsStep(float dt)
@@ -68,7 +88,10 @@ public class CustomRigidBody : MonoBehaviour
         ClearForces();
 
         //현재 상태 업데이트
-        currentState.position = new Vec3(transform.position.x,transform.position.y,transform.position.z);
+        currentState.position += deltaPosition;
+
+        //Nan방지
+        Sanitize();
     }
 
     /// <summary>
@@ -123,5 +146,23 @@ public class CustomRigidBody : MonoBehaviour
     void IntegratePosition(Vec3 velocity, float dt)
     {
         deltaPosition = (velocity * dt);
+    }
+
+    /// <summary>
+    /// Nan 방지
+    /// </summary>
+    void Sanitize()
+    {
+        if (float.IsNaN(velocity.x) || float.IsInfinity(velocity.x)) velocity.x = 0;
+        if (float.IsNaN(velocity.y) || float.IsInfinity(velocity.y)) velocity.y = 0;
+        if (float.IsNaN(velocity.z) || float.IsInfinity(velocity.z)) velocity.z = 0;
+
+        if (float.IsNaN(acceleration.x) || float.IsInfinity(acceleration.x)) acceleration.x = 0;
+        if (float.IsNaN(acceleration.y) || float.IsInfinity(acceleration.y)) acceleration.y = 0;
+        if (float.IsNaN(acceleration.z) || float.IsInfinity(acceleration.z)) acceleration.z = 0;
+
+        if (float.IsNaN(deltaPosition.x) || float.IsInfinity(deltaPosition.x)) deltaPosition.x = 0;
+        if (float.IsNaN(deltaPosition.y) || float.IsInfinity(deltaPosition.y)) deltaPosition.y = 0;
+        if (float.IsNaN(deltaPosition.z) || float.IsInfinity(deltaPosition.z)) deltaPosition.z = 0;
     }
 }
