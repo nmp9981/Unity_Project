cf9bc41c6ecfe23cbbfabe7074d3075299f8aafc
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-12-05 01:17:22 +0900

    Create CustomPhysicsManager.cs

---
+++ Study/Physics/4th_Month/CustomPhysicsManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+enum Dimension { TwoD, ThreeD }
+
+public class CustomPhysicsManager : MonoBehaviour
+{
+    [SerializeField]
+    Dimension mode;//2D, 3D 결정
+
+    public PhysicsWorld2D world2D;
+    public PhysicsWorld world3D;
+    public float fixedDt = 0.02f;
+
+    void Start()
+    {
+        world2D = new PhysicsWorld2D();
+        world3D = new PhysicsWorld();
+        SceneBootstrap(mode);    // rigidbody & collider 생성
+    }
+
+    void FixedUpdate()
+    {
+        if (mode == Dimension.TwoD)
+        {
+            if (!world2D.physicsPaused)
+                world2D.PhysicsStep(fixedDt);
+        }
+        else
+        {
+            if (!world3D.physicsPaused)
+                world3D.PhysicsStep(fixedDt);
+        }
+    }
+
+    /// <summary>
+    /// rigidbody & collider 생성
+    /// </summary>
+    /// <param name="world"></param>
+    void SceneBootstrap(Dimension mode)
+    {
+        if (mode == Dimension.TwoD) {
+            world2D.Create_ColliderAndRigid2D();
+        }
+        else
+        {
+            world3D.Create_ColliderAndRigid3D();
+        }
+    }
+}
