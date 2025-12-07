15325aa20f727be3139b905a842f21932d566b61
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-12-07 13:09:31 +0900

    Update IntegrationUtility.cs

--- Study/Physics/4th_Month/IntegrationUtility.cs
+++ Study/Physics/4th_Month/IntegrationUtility.cs
@@ -1,7 +1,7 @@
 public static class IntegrationUtility
 {
     /// <summary>
-    /// Verlet 위치 수치적분
+    /// Verlet 위치 수치적분 - 지울 예정
     /// </summary>
     /// <param name="dt">시간 간격</param>
     /// <param name="a">가속도</param>
@@ -12,5 +12,33 @@ public static class IntegrationUtility
     {
         float nextX = 2 * curX - preX + a * dt * dt;
         return nextX;
+    }
+
+    /// <summary>
+    /// Verlet 수치 적분 : 상태 반환
+    /// </summary>
+    /// <param name="dt">시간</param>
+    /// <param name="curState">현재 상태</param>
+    /// <param name="m">질량</param>
+    /// <param name="force">힘</param>
+    /// <returns></returns>
+    public static RigidbodyState VelocityVerlet(float dt, RigidbodyState curState, Vec3 force, float mass)
+    {
+        //위치 업데이트
+        Vec3 newX = curState.position + curState.velocity * dt + curState.accel * dt * dt*0.5f;
+
+        //가속도 업데이트
+        Vec3 newA = force / mass;
+
+        //속도 업데이트
+        Vec3 newV = curState.velocity+(curState.accel+newA)*dt*0.5f;
+
+        //다음 상태 반환
+        return new RigidbodyState
+        {
+            position = newX,
+            velocity = newV,
+            accel = newA,
+        };
     }
 }
