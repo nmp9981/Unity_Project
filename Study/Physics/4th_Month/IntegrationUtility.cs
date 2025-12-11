3581d1a583c6abefb913fb726bfa876b1de4c7db
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-12-12 01:59:09 +0900

    Update IntegrationUtility.cs

--- Study/Physics/4th_Month/IntegrationUtility.cs
+++ Study/Physics/4th_Month/IntegrationUtility.cs
@@ -1,3 +1,5 @@
+using System.Runtime.ConstrainedExecution;
+
 /// <summary>
 /// 위치 속도 미분 - 2차원
 /// </summary>
@@ -42,14 +44,18 @@ public static class IntegrationUtility
     /// <returns></returns>
     public static RigidbodyState VelocityVerlet(float dt, RigidbodyState curState, Vec3 force, float mass)
     {
+        Vec3 x = curState.position;
+        Vec3 v = curState.velocity;
+        Vec3 a = curState.accel;   // 이전 스텝 가속도
+
         //위치 업데이트
-        Vec3 newX = curState.position + curState.velocity * dt + curState.accel * dt * dt*0.5f;
+        Vec3 newX = x + v * dt + a * dt * dt*0.5f;
 
         //가속도 업데이트
-        Vec3 newA = force / mass;
+        Vec3 newA = PhysicsFormula.Force_HookLaw(newX) / mass;
 
         //속도 업데이트
-        Vec3 newV = curState.velocity+(curState.accel+newA)*dt*0.5f;
+        Vec3 newV = v + (a + newA)*0.5f * dt;
 
         //다음 상태 반환
         return new RigidbodyState
@@ -106,11 +112,11 @@ public static class IntegrationUtility
     /// 미분
     /// </summary>
     /// <returns></returns>
-    private static Derivative Evaluate(RigidbodyState state, Vec3 force, float mass)
+    private static Derivative Evaluate(RigidbodyState state, float mass)
     {
         Derivative d;
         d.dPosition = state.velocity;//위치 미분하면 속도
-        d.dVelocity = force / mass;//속도 미분하면 가속도
+        d.dVelocity = PhysicsFormula.Force_HookLaw(state.position) / mass;//속도 미분하면 가속도
         return d;
     }
     /// <summary>
@@ -162,28 +168,28 @@ public static class IntegrationUtility
     /// <returns></returns>
     public static RigidbodyState IntegrateRK4(RigidbodyState state, Vec3 force, float mass, float dt)
     {
-        Derivative k1= Evaluate(state, force, mass);
+        Derivative k1= Evaluate(state, mass);
 
         RigidbodyState state2 = new RigidbodyState
         {
             position = state.position + k1.dPosition * 0.5f * dt,
             velocity = state.velocity + k1.dVelocity * 0.5f * dt
         };
-        Derivative k2 = Evaluate(state2, force, mass);
+        Derivative k2 = Evaluate(state2, mass);
 
         RigidbodyState state3 = new RigidbodyState
         {
             position = state.position + k2.dPosition * 0.5f * dt,
             velocity = state.velocity + k2.dVelocity * 0.5f * dt
         };
-        Derivative k3 = Evaluate(state3, force, mass);
+        Derivative k3 = Evaluate(state3, mass);
 
         RigidbodyState state4 = new RigidbodyState
         {
             position = state.position + k3.dPosition * dt,
             velocity = state.velocity + k3.dVelocity * dt
         };
-        Derivative k4 = Evaluate(state4, force, mass);
+        Derivative k4 = Evaluate(state4, mass);
 
         //가중치 합
         state.position += (k1.dPosition + k2.dPosition*2 + k3.dPosition*2 + k4.dPosition) * dt / 6;
