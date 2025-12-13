d489433a2c28d8f916639f545cf716ce87585a57
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-12-13 21:54:08 +0900

    Update PhysicsFormula.cs

--- Study/Physics/4th_Month/PhysicsFormula.cs
+++ Study/Physics/4th_Month/PhysicsFormula.cs
@@ -2,12 +2,22 @@
 public static class PhysicsFormula
 {
     /// <summary>
-    /// 훅의 법칙 적용해 힘 구하기
+    /// 훅의 법칙 적용해 힘 구하기 - 3차원
     /// </summary>
     /// <param name="position"></param>
     /// <returns></returns>
     public static Vec3 Force_HookLaw(Vec3 position)
     {
         return position * (-1)*TestScripts.k;
+    }
+    /// <summary>
+    /// 훅의 법칙 적용해 힘 구하기 - 2차원
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns></returns>
+    public static Vec2 Force_HookLaw2D(Vec3 position)
+    {
+        Vec2 pos = new Vec2(position.x, position.y);
+        return pos * (-1) * TestScripts.k;
     }
 }
