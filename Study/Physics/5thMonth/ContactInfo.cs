9fde10f19d02bc46dad6e576ce40943cde718f1f
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-12-18 01:58:09 +0900

    Update ContactInfo.cs

--- Study/Physics/5thMonth/ContactInfo.cs
+++ Study/Physics/5thMonth/ContactInfo.cs
@@ -3,13 +3,19 @@
 /// </summary>
 public class ContactInfo
 {
+    //두 충돌체
+    public CustomRigidBody rigidA;
+    public CustomRigidBody rigidB;
+
     public bool isCollide;//충돌 여부
     public Vec3 normal;//어느 방향으로 밀어낼지
     public float penetration;//겹침량
     public Vec3 tangent;//탄젠트 벡터
+    public Vec3 contactPoint;// 회전
 
     public float normalImpulse;   // 누적 Jn
     public float tangentImpulse;  // 누적 Jt
 
-    public float invMassSum;
+    public float invMassSum;//두 물체의 질량 역수의 합
+    public float frictionValue;//마찰 계수
 }
