fcdf16a37a4f6274c79f8ac5b3324e265c986e93
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-12-23 02:12:43 +0900

    Update IslandBuilder.cs

--- Study/Physics/5thMonth/IslandBuilder.cs
+++ Study/Physics/5thMonth/IslandBuilder.cs
@@ -1,6 +1,4 @@
-using NUnit.Framework;
 using System.Collections.Generic;
-using TMPro;
 
 public static class IslandBuilder
 {
@@ -30,5 +28,82 @@ public static class IslandBuilder
             islands.Add(island);
         }
         return islands;
+    }
+
+    /// <summary>
+    /// 물체 그룹을 묶는 함수(물체와 연결된 그래프 그리기)
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="island"></param>
+    /// <param name="manifolds"></param>
+    /// <param name="visited"></param>
+    public static void BuildIslandDFS(
+                CustomRigidBody start,
+                Island island,
+                List<ContactManifold> manifolds,
+                HashSet<CustomRigidBody> visited
+            )
+    {
+        var stack = new Stack<CustomRigidBody>();
+        stack.Push(start);
+        visited.Add(start);
+
+        while (stack.Count > 0)
+        {
+            var body = stack.Pop();
+            island.bodies.Add(body);
+
+            foreach(var manifold in manifolds)
+            {
+                //이미 방문 함
+                if(!ManifoldContains(manifold, body)) continue;
+
+                //중복 방지를 위해 manifold는 island에 추가
+                if (island.manifolds.Contains(manifold))
+                {
+                    island.bodies.Add(body);
+                }
+
+                //붙어있는 다른 물체
+                var other = GetOtherBody(manifold, body);
+
+                //static body, 정적 물체는 island의 멤버가 아님
+                if (other==null || other.isStatic)
+                {
+                    continue;
+                }
+
+                //미방문
+                if (!visited.Contains(other))
+                {
+                    //방문
+                    visited.Add(other);
+                    stack.Push(other);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 이미 방문 했는가?
+    /// </summary>
+    /// <param name="m"></param>
+    /// <param name="body"></param>
+    /// <returns></returns>
+    static bool ManifoldContains(ContactManifold m, CustomRigidBody body)
+    {
+        return m.rigidA == body || m.rigidB == body;
+    }
+    /// <summary>
+    /// 다른 물체 반환
+    /// </summary>
+    /// <param name="m"></param>
+    /// <param name="body"></param>
+    /// <returns></returns>
+    static CustomRigidBody GetOtherBody(ContactManifold m, CustomRigidBody body)
+    {
+        if (m.rigidA == body) return m.rigidB;
+        if (m.rigidB == body) return m.rigidA;
+        return null;
     }
 }
