3516a537fb6b6e9a305d969856dea9c01fb6eea0
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-12-30 22:41:55 +0900

    Create JointSolver.cs

---
+++ Study/Physics/6thMonth/JointSolver.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+
+public class JointSolver : MonoBehaviour, IConstraint
+{
+    float dt;
+    public void SolveJoint()
+    {
+        foreach (contact)
+        {
+            SolveVelocity(dt);
+        }
+        foreach (contact)
+        {
+            SovlePosition(dt);
+        }
+    }
+
+    public void SolveVelocity(float dt)
+    {
+        Vector3 pA = bodyA.position + rA;
+        Vector3 pB = bodyB.position + rB;
+
+        Vector3 n = pB - pA;
+        float dist = n.magnitude;
+        n /= dist;
+
+        // 상대 속도
+        Vector3 vRel =
+            bodyB.velocity + Vector3.Cross(bodyB.angularVelocity, rB) -
+            bodyA.velocity - Vector3.Cross(bodyA.angularVelocity, rA);
+
+        // bias
+        float C = dist - restLength;
+        float b = beta / dt * C;
+
+        // λ
+        float lambda = -(Vector3.Dot(n, vRel) + b) * effectiveMass;
+
+        // impulse
+        Vector3 impulse = lambda * n;
+
+        // 적용
+        bodyA.velocity -= impulse * bodyA.invMass;
+        bodyB.velocity += impulse * bodyB.invMass;
+
+        bodyA.angularVelocity -= bodyA.invInertia * Vector3.Cross(rA, impulse);
+        bodyB.angularVelocity += bodyB.invInertia * Vector3.Cross(rB, impulse);
+    }
+    public void SovlePosition(float dt)
+    {
+        Vector3 correction = C * n;
+
+        bodyA.position += correction * bodyA.invMass / totalInvMass;
+        bodyB.position -= correction * bodyB.invMass / totalInvMass;
+    }
+}
