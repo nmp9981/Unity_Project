12e3387713e68a5e2ae7274dbeed21e3fe7a9ba7
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2026-01-03 16:51:12 +0900

    Update Joint.cs

--- Study/Physics/6thMonth/Joint.cs
+++ Study/Physics/6thMonth/Joint.cs
@@ -3,6 +3,7 @@ abstract class Joint
     public CustomRigidBody rigidA;
     public CustomRigidBody rigidB;
 
-    public abstract void SolveVelocity();
-    public abstract void SolvePosition();
+    public abstract void SolveVelocity(float dt);
+    public abstract void SolvePosition(float dt);
+    public abstract void WarmStart();
 }
