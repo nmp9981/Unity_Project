fc9f019be659477377ae0deec5156c249c9c866c
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2026-01-11 16:09:06 +0900

    Update Joint.cs

--- Study/Physics/6thMonth/Joint.cs
+++ Study/Physics/6thMonth/Joint.cs
@@ -1,9 +1,22 @@
-abstract class Joint
+using System.Collections.Generic;
+// Joint
+// - ConstraintRow를 생성한다
+// - Solver 로직 없음
+// - RigidBody 상태 직접 변경 금지
+public abstract class Joint
 {
-    public CustomRigidBody rigidA;
-    public CustomRigidBody rigidB;
+    //Joint 고유 번호
+    public int jointId;
 
-    public abstract void SolveVelocity(float dt);
-    public abstract void SolvePosition(float dt);
-    public abstract void WarmStart();
+    // Joint 내부에서만 사용 (Jacobian 계산용)
+    protected CustomRigidBody rigidA;
+    protected CustomRigidBody rigidB;
+
+    protected readonly List<ConstraintRow> constraintRows
+        = new List<ConstraintRow>();
+
+    /// <summary>
+    /// Jacobian / effectiveMass / bias 정의
+    /// </summary>
+    public abstract void BuildConstraintRows(float dt);
 }
