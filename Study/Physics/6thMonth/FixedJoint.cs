1d6349dded6744b54e58ba27c6c6fe781bf2e07f
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2026-01-16 02:41:41 +0900

    Update FixedJoint.cs

--- Study/Physics/6thMonth/FixedJoint.cs
+++ Study/Physics/6thMonth/FixedJoint.cs
@@ -2,14 +2,26 @@ class FixedJoint : Joint
 {
     public override void BuildConstraintRows(float dt)
     {
-        // 위치 완전 고정
-        AddLinearLockRow(X);
-        AddLinearLockRow(Y);
-        AddLinearLockRow(Z);
+        // 앵커
+        Vec3 worldA = rigidA.LocalToWorld(localAnchorA);
+        Vec3 worldB = rigidB.LocalToWorld(localAnchorB);
 
-        // 회전 완전 고정
-        AddAngularLockRow(X);
-        AddAngularLockRow(Y);
-        AddAngularLockRow(Z);
+        rA = worldA - rigidA.position;
+        rB = worldB - rigidB.position;
+
+        // A 기준 축
+        Vec3 x = rigidA.rotation * VectorMathUtils.RightVector3D();
+        Vec3 y = rigidA.rotation * VectorMathUtils.UpVector3D();
+        Vec3 z = rigidA.rotation * VectorMathUtils.FrontVector3D();
+
+        // 위치 고정
+        AddLinearLockRow(x);
+        AddLinearLockRow(y);
+        AddLinearLockRow(z);
+
+        // 회전 고정
+        AddAngularLockRow(x);
+        AddAngularLockRow(y);
+        AddAngularLockRow(z);
     }
 }
