5976f560bba469b28cf82164baa25f8a1d647387
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2026-01-16 14:19:05 +0900

    Update DistanceJoint.cs

--- Study/Physics/6thMonth/DistanceJoint.cs
+++ Study/Physics/6thMonth/DistanceJoint.cs
@@ -1,15 +1,30 @@
 class DistanceJoint : Joint
 {
+    float minDistance;
+    float maxDistance;
+
     public override void BuildConstraintRows(float dt)
     {
-        // 선형: 전부 Lock
-        AddLinearLockRow(X);
-        AddLinearLockRow(Y);
-        AddLinearLockRow(Z);
+        // 앵커
+        Vec3 worldA = rigidA.LocalToWorld(localAnchorA);
+        Vec3 worldB = rigidB.LocalToWorld(localAnchorB);
 
-        // 회전: 전부 Free
-        AddAngularFreeRow(X);
-        AddAngularFreeRow(Y);
-        AddAngularFreeRow(Z);
+        rA = worldA - rigidA.position;
+        rB = worldB - rigidB.position;
+
+        Vec3 delta = worldB - worldA;
+        float dist = delta.Magnitude;
+
+        //거리 0
+        if (dist < 1e-6f)
+            return;
+
+        //방향
+        Vec3 n = delta / dist;
+
+        // 거리 제한 (양방향)
+        AddLinearLimitRow(n,dist,minDistance,maxDistance);
+
+        // 회전은 완전 자유 (아무것도 추가하지 않음)
     }
 }
