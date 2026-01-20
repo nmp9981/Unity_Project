a19b5e3d384b48c1d1b87c28acd8cd4d1e09ee89
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2026-01-20 21:49:33 +0900

    Create Physicsworld.cs

---
+++ Study/Physics/7thMonth_Vehicle/Physicsworld.cs
@@ -0,0 +1,286 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsWorld : MonoBehaviour
+{
+    [SerializeField]
+    List<CustomRigidBody> rigidBodies3D = new List<CustomRigidBody>();
+    [SerializeField]
+    List<CustomCollider3D> colliders3D = new List<CustomCollider3D>();
+
+    //물리 작동 여부
+    public bool physicsPaused = false;
+
+    //지면 판정
+    public float groundThreshold;
+
+    //충돌 정보 모음
+    public List<ContactInfo> contactList = new List<ContactInfo>();//이건 사용하지 않음
+    //Manifold 리스트, 이게 충돌 정보
+    public List<ContactManifold> manifolds = new();
+    
+    private void Update()
+    {
+        // P 버튼으로 물리만 멈춤/재시작
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            physicsPaused = !physicsPaused;
+        }
+
+        if (!physicsPaused)
+            PhysicsStep(Time.deltaTime);
+
+        RenderStep();
+    }
+
+    #region Physics Step Pipeline
+    /// <summary>
+    /// 물리 스텝
+    /// Physics Step → Collision → Resolve → Commit -> Sleep → Render Step(Interpolation) 
+    /// </summary>
+    /// <param name="dt"></param>
+    public void PhysicsStep(float dt)
+    {
+        PredictBodies(dt);
+        DetectCollisions();
+        BuildManifolds();
+        AccumulateGroundContacts();
+        SolveIslands(dt);
+        CommitBodies(dt);
+        UpdateSleeping();
+    }
+    #endregion
+
+    /// <summary>
+    /// 1. Predict : 예측
+    /// </summary>
+    /// <param name="dt"></param>
+    void PredictBodies(float dt)
+    {
+        foreach (var rb in rigidBodies3D)
+        {
+            rb.isGrounded = false;
+            rb.PredictState(dt);
+        }
+    }
+    /// <summary>
+    /// 2. Collision Detection : 충돌 체크
+    /// contactList 생성, 내부에서 PositionalCorrection만 수행
+    /// </summary>
+    void DetectCollisions()
+    {
+        contactList.Clear();
+
+        for (int i = 0; i < colliders3D.Count; i++)
+        {
+            for (int j = i + 1; j < colliders3D.Count; j++)
+            {
+                var colA = colliders3D[i];
+                var colB = colliders3D[j];
+
+                if (IslandSleepSystem.CanSkipCollision(colA.rigidBody, colB.rigidBody))
+                    continue;
+
+                if (!ColiisionUtility.IsCollisionAABB3D(colA, colB))
+                    continue;
+
+                contactList.Add(
+                    ColiisionUtility.GetContactAABB3D(colA, colB)
+                );
+            }
+        }
+    }
+    /// <summary>
+    /// 3. Manifold Update
+    /// </summary>
+    void BuildManifolds()
+    {
+        ContactSolver.UpdateManifolds(manifolds,contactList,out manifolds);
+    }
+    /// <summary>
+    /// 4.Ground 증거 수집
+    /// </summary>
+    void AccumulateGroundContacts()
+    {
+        foreach (var manifold in manifolds)
+        {
+            foreach (var cp in manifold.points)
+            {
+                if (cp.contactNormal.y > groundThreshold)
+                {
+                    manifold.rigidA.hasGroundContact = true;
+
+                    if (cp.contactNormal.y > manifold.rigidA.groundNormal.y)
+                        manifold.rigidA.groundNormal = cp.contactNormal;
+                }
+
+                if (cp.contactNormal.y < -groundThreshold)
+                {
+                    manifold.rigidB.hasGroundContact = true;
+
+                    if (-cp.contactNormal.y > manifold.rigidB.groundNormal.y)
+                        manifold.rigidB.groundNormal = cp.contactNormal * (-1);
+                }
+            }
+        }
+    }
+   
+    /// <summary>
+    /// 5. Solve
+    /// Solver는 Island 단위로 돌린다
+    /// </summary>
+    /// <param name="dt"></param>
+    void SolveIslands(float dt)
+    {
+        //접촉점 그룹
+        var islands = IslandBuilder.Build(rigidBodies3D, manifolds);
+
+        foreach (var island in islands)
+        {
+            //sleeping 상태
+            if (island.isSleeping)
+                continue;
+
+            WarmStartIsland(island, dt);
+            SolveIsland(island, dt);
+            //Impulse값 저장
+            ContactSolver.SaveImpulse(island);
+        }
+    }
+    /// <summary>
+    /// WarmStart 시작 
+    /// </summary>
+    /// <param name="island"></param>
+    /// <param name="dt"></param>
+    void WarmStartIsland(Island island, float dt)
+    {
+        //지난 프레임에 이미 구해놓은 impulse를 이번 프레임 Solver 시작 전에 미리 적용
+        foreach (var manifold in island.manifolds)
+        {
+            foreach (var cp in manifold.points)
+            {
+                //지난 프레임에 수렴한 해를 이번 프레임의 초기값으로 재사용
+                if(!cp.isSpeculative)
+                    ContactSolver.WarmStart(manifold, cp);
+            }
+        }
+        // 🔥 Joint Warm Start
+        foreach (var joint in island.joints)
+        {
+            joint.WarmStart(dt);
+        }
+    }
+    /// <summary>
+    /// 속도, 위치 Solver
+    /// </summary>
+    /// <param name="island"></param>
+    /// <param name="dt"></param>
+    void SolveIsland(Island island, float dt)
+    {
+        // 5. Velocity Solver, 속도 -> 위치
+        ContactSolver.SolveVelocityConstraints(island.manifolds, dt);
+        // 6. Contact Solver (GS Iteration)
+        ContactSolver.SolvePositionConstraints(island.manifolds, dt);
+    }
+    /// <summary>
+    /// 6. Commit
+    /// </summary>
+    /// <param name="dt"></param>
+    void CommitBodies(float dt)
+    {
+        foreach (var rb in rigidBodies3D)
+            rb.Commit(dt);
+    }
+   /// <summary>
+   /// 7. Sleeping
+   /// </summary>
+    void UpdateSleeping()
+    {
+        IslandSleepSystem.UpdateSleeping(IslandBuilder.Build(rigidBodies3D, manifolds));
+    }
+
+    public void Create_ColliderAndRigid3D()
+    {
+        rigidBodies3D.AddRange(FindObjectsByType<CustomRigidBody>(FindObjectsSortMode.None));
+        colliders3D.AddRange(FindObjectsByType<CustomCollider3D>(FindObjectsSortMode.None));
+    }
+
+    /// <summary>
+    /// 렌더링 스텝
+    /// 렌더링은 물리 상태를 읽기만 한다.
+    /// 위치 상태 업데이트
+    /// </summary>
+    /// <param name="dt"></param>
+    void RenderStep()
+    {
+        float alpha = (float)(Time.timeAsDouble - Time.fixedTimeAsDouble) / Time.fixedDeltaTime;
+        float alpha01 = MathUtility.ClampValue(alpha, 0, 1);//보간 값
+
+        foreach (var rb in rigidBodies3D)
+        {
+            Vec3 lerped = Vec3.Lerp(rb.previousState.position, rb.currentState.position, alpha01);
+
+            rb.transform.position = new Vector3(lerped.x, lerped.y, lerped.z);
+        }
+    }
+    /// <summary>
+    /// RayCast 3D
+    /// </summary>
+    /// <param name="ray"></param>
+    /// <param name="maxDistance"></param>
+    /// <param name="hit"></param>
+    /// <param name="layerMask"></param>
+    /// <returns></returns>
+    public bool Raycast(Ray3D ray,float maxDistance,out RaycastHit3D hit,int layerMask = ~0)
+    {
+        bool hasHit = false;
+        float bestT = maxDistance;
+        RaycastHit3D bestHit = default;
+
+        foreach (var col in colliders3D)
+        {
+            if (((1 << col.layer) & layerMask) == 0)
+                continue;
+
+            if (col.RayCast(ray, bestT, out RaycastHit3D tempHit))
+            {
+                if (tempHit.t < bestT)
+                {
+                    bestT = tempHit.t;
+                    bestHit = tempHit;
+                    hasHit = true;
+                }
+            }
+        }
+
+        hit = bestHit;
+        return hasHit;
+    }
+    /// <summary>
+    /// 조건 만족하는 object전부 소환
+    /// </summary>
+    /// <param name="ray"></param>
+    /// <param name="maxDistance"></param>
+    /// <param name="results"></param>
+    /// <param name="layerMask"></param>
+    /// <returns></returns>
+    public int RaycastAll(Ray3D ray,float maxDistance,List<RaycastHit3D> results, int layerMask = ~0)
+    {
+        results.Clear();
+
+        foreach (var col in colliders3D)
+        {
+            if (((1 << col.layer) & layerMask) == 0)
+                continue;
+
+            if (col.RayCast(ray, maxDistance, out RaycastHit3D hit))
+            {
+                results.Add(hit);
+            }
+        }
+        //거리순 정렬
+        results.Sort((a, b) => a.t.CompareTo(b.t));
+
+        return results.Count;
+    }
+}
