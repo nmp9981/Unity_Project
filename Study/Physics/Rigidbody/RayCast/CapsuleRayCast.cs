e946f16a6c06c7fa81176ca9ca152f368cd34a01
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2026-01-21 20:50:37 +0900

    Update CapsuleRayCast.cs

--- Study/Physics/Rigidbody/RayCast/CapsuleRayCast.cs
+++ Study/Physics/Rigidbody/RayCast/CapsuleRayCast.cs
@@ -49,13 +49,15 @@ public class CapsuleRayCast : CustomCollider3D
                 for (int i = 0; i < 2; i++)
                 {
                     float t = (i == 0) ? t0 : t1;
+                    //해 허용 범위를 넘김
                     if (t < 0.0f || t > bestT) continue;
 
+                    //원통 내에 있는지 판정
                     float s = (mn + t * nd) / nn;
                     if (s >= 0.0f && s <= 1.0f)
                     {
-                        Vec3 hitPos = ray.origin + t * d;
-                        Vec3 proj = pointA + s * n;
+                        Vec3 hitPos = ray.origin + t * d;//부딪힌 지점
+                        Vec3 proj = pointA + s * n;//n에 투영
 
                         Vec3 normal = (hitPos - proj).Normalized;
 
@@ -77,5 +79,49 @@ public class CapsuleRayCast : CustomCollider3D
 
         hit = bestHit;
         return hasHit;
+    }
+
+    /// <summary>
+    /// Circle Raycast
+    /// normal은 오직 tMin이 갱신될 때만 바뀐다
+    /// </summary>
+    /// <param name="ray"></param>
+    /// <param name="maxT">ray 최대 거리</param>
+    /// <param name="hit">부딪힌 물체</param>
+    /// <returns></returns>
+    public void CheckSphere(Vec3 center, float radius, Ray3D ray, ref float bestT, ref RaycastHit3D bestHit, ref bool hasHit)
+    {
+        //2차 방정식으로 변환
+        Vec3 m = ray.origin - center;
+        Vec3 d = ray.dir;
+
+        float a = Vec3.Dot(d, d); // 정규화 시 = 1
+        float b = Vec3.Dot(d, m);
+        float c = Vec3.Dot(m, m) - radius * radius;
+
+        //2차 방정식의 판별식
+        float discr = b * b - a * c;
+        if (discr < 0.0f) return;//허근, 실수해 없음
+
+        //실수해
+        float sqrtD = MathUtility.Root(discr);
+        float t0 = (-b - sqrtD) / a;//min
+        float t1 = (-b + sqrtD) / a;//max
+
+        // Ray 뒤쪽
+        if (t1 < 0.0f) return;
+
+        float t = (t0 >= 0.0f) ? t0 : t1;
+        if (t < 0.0f || t > bestT) return;
+
+        Vec3 hitPos = ray.origin + t * d;
+        Vec3 normal = (hitPos - center).Normalized;
+
+        bestT = t;
+        bestHit.t = t;
+        bestHit.position = hitPos;
+        bestHit.normal = normal;
+        bestHit.collider = this;
+        hasHit = true;
     }
 }
