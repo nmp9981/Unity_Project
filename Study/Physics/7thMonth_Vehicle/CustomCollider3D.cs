aa603698b2e9cccd0dad51dc1039e2d66fdafe27
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2026-01-26 17:30:50 +0900

    Update CustomCollider3D.cs

--- Study/Physics/7thMonth_Vehicle/CustomCollider3D.cs
+++ Study/Physics/7thMonth_Vehicle/CustomCollider3D.cs
@@ -87,6 +87,54 @@ public abstract class CustomCollider3D : MonoBehaviour
         float zMax = transform.position.z + size.z * 0.5f;
         return new Vec3(xMax, yMax,zMax);
     }
+/// <summary>
+/// Narraw Coliider 판정
+/// 대략적인 충돌 범위내에 들어가는지 판정
+/// </summary>
+/// <param name="ray"></param>
+/// <param name="min"></param>
+/// <param name="max"></param>
+/// <param name="maxT"></param>
+/// <param name="tEnter"></param>
+/// <returns></returns>
+public bool RaycastAABB(Ray3D ray,Vec3 min,Vec3 max,float maxT,out float tEnter)
+{
+    tEnter = 0.0f;
+    float tExit = maxT;
+
+    for (int i = 0; i < 3; i++)
+    {
+        float origin = ray.origin.Array[i];
+        float dir = ray.dir.Array[i];
+
+        if (Math.Abs(dir) < MathUtility.EPSILON)
+        {
+            // Ray가 slab과 평행
+            if (origin < min.Array[i] || origin > max.Array[i])
+                return false;
+        }
+        else
+        {
+            float invD = 1.0f / dir;
+            float t1 = (min.Array[i] - origin) * invD;
+            float t2 = (max.Array[i] - origin) * invD;
+
+            if (t1 > t2)
+            {
+                float tmp = t1;
+                t1 = t2;
+                t2 = tmp;
+            }
+
+            tEnter = Math.Max(tEnter, t1);
+            tExit = Math.Min(tExit, t2);
+
+            if (tEnter > tExit)
+                return false;
+        }
+    }
+    return true;
+}
 
     public abstract bool RayCast(Ray3D ray, float maxT, out RaycastHit3D hit);
 }
