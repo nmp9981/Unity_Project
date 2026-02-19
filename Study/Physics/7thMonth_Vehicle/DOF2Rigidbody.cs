619ca928425c35d8a12abfb6245c53938571a7b9
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2026-02-19 14:25:17 +0900

    Update DOF2Rigidbody.cs

--- Study/Physics/7thMonth_Vehicle/DOF2Rigidbody.cs
+++ Study/Physics/7thMonth_Vehicle/DOF2Rigidbody.cs
@@ -1,20 +1,28 @@
-using NUnit.Framework;
 using System.Data;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class DOF2Rigidbody : MonoBehaviour
 {
     float delta;
-    float vy,ax;
-    float a, b, r;
-    float Ux;
+    float vy,Ux;
+    float currentUx,prevUx;
+    float a, b,r;
     float h, L, m;
     float FzFront, FzRear, staticFront, staticRear;
     float mu, Cf, Cr;
     float vy_dot, r_dot,Iz;
+    Vector3 local;
 
     CustomRigidBody rb;
-   
+
+    private void Start()
+    {
+        Vector3 velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z);
+        local = transform.InverseTransformDirection(velocity);
+        vy = local.y;
+        r = rb.angularVelocity.y;
+    }
     private void FixedUpdate()
     {
         DOF2Flow(Time.fixedDeltaTime);
@@ -22,20 +30,29 @@ public class DOF2Rigidbody : MonoBehaviour
     void DOF2Flow(float dt)
     {
         //1.현재 속도 추출
+        Vector3 velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z);
+        local = transform.InverseTransformDirection(velocity);
+        Ux = local.z;
+        prevUx = currentUx;
+        currentUx = Ux;
         //2.슬립각 계산
-        float alphaF = delta - (vy + a * r) / Ux;
-        float alphaR = (b * r - vy) / Ux;
+        float U = Mathf.Max(Mathf.Abs(Ux), 0.1f);
+        float alphaF = delta - (vy + a * r) / U;
+        float alphaR = (b * r - vy) / U;
         //3.하중 이동 계산
+        float ax = (currentUx - prevUx) / dt;
         float transfer = (h / L) * m * ax;
 
         FzFront = staticFront + transfer;
+        FzFront = MathUtility.Max(FzFront, 0);
         FzRear = staticRear - transfer;
+        FzRear = MathUtility.Max(FzRear,0);
 
         //4.타이어 횡력 계산(비선형)
         float FyF = mu * FzFront * MathUtility.Tanh(Cf * alphaF / (mu * FzFront));
-        float FyR = mu * FzRear * MathUtility.Tanh(Cr * alphaF / (mu * FzRear));
+        float FyR = mu * FzRear * MathUtility.Tanh(Cr * alphaR / (mu * FzRear));
         //5.차량 횡력 / 요 모멘트 계산
-        vy_dot = (FyF + FyR) / m - Ux * r;
+        vy_dot = (FyF + FyR) / m - U * r;
         r_dot = (a * FyF - b * FyR) / Iz;
         //6.상태 미분 계산
         //7.적분(Euler 또는 RK2)
