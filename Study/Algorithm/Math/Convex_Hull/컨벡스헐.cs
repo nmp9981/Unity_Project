bb911611a5f14520a5ef3f79ec49d34cbf389cc4
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2026-05-03 11:34:58 +0900

    Update 컨벡스헐.cs

--- Study/Algorithm/Math/Convex_Hull/컨벡스헐.cs
+++ Study/Algorithm/Math/Convex_Hull/컨벡스헐.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace ConVexHULL
@@ -14,39 +15,91 @@ namespace ConVexHULL
         //도형을 이루는 최종 점들의 집합
         Stack<Point> shapePointStack = new Stack<Point>();
         //기준점
-        Point pivotPoint;
+        int pivotIndex = 0;
 
         /// <summary>
         /// 전체 과정
         /// </summary>
         public void AllFlow(List<Point> pointList)
         {
-            if (pointList.Count < 3) return;//도형을 이룰 수 없음
+            //다각형을 이룰 수 없음
+            if (pointList.Count < 3) return;
 
-            //기준점 잡기
-            pivotPoint = pointList[0];
-            foreach (var pos in pointList)
+            //피벗 점 찾기
+            for (int i = 1; i < pointList.Count; i++)
             {
-                if (pivotPoint.y > pos.y || (pivotPoint.y == pos.y && pivotPoint.x > pos.x))
+                //y좌표가 더 작은 점이면 갱신 같은 경우 x좌표가 더 작은 값으로
+                if (pointList[pivotIndex].y > pointList[i].y ||
+                    (pointList[pivotIndex].y == pointList[i].y && pointList[pivotIndex].x > pointList[i].x))
                 {
-                    pivotPoint = pos;
+                    pivotIndex = i;
                 }
             }
+
             //각도 정렬
-            pointList.Sort((a, b) => {
-                if (a.Equals(pivotPoint)) return -1;
-                if (b.Equals(pivotPoint)) return 1;
+            pointList = AngleSort(pointList);
 
-                int dir = CCW(pivotPoint, a, b);
-                if (dir != 0) return -dir; // 반시계 방향이 우선순위가 높도록
+            //일직선 위 점 제거하면서 컨벡스헐 구성
+            RemovePointInLine(pointList);
+        }
 
-                // 일직선상에 있다면 거리가 가까운 쪽을 우선
-                float distA = Mathf.Pow(a.x - pivotPoint.x, 2) + Mathf.Pow(a.y - pivotPoint.y, 2);
-                float distB = Mathf.Pow(b.x - pivotPoint.x, 2) + Mathf.Pow(b.y - pivotPoint.y, 2);
-                return distA.CompareTo(distB);
-            });
+        /// <summary>
+        /// 각도 정렬
+        /// </summary>
+        public List<Point> AngleSort(List<Point> pointList)
+        {
+            //배열로 바꿈
+            Point[] pointArray = pointList.ToArray();
 
-            RemovePointInLine(pointList);//일직선 위 점 제거
+            //피벗포인트를 0번으로
+            Point temp = pointArray[0];
+            pointArray[0] = pointArray[pivotIndex];
+            pointArray[pivotIndex] = temp;
+
+            //각도 정렬
+            int pointLength = pointArray.Length;
+            bool isSwap = false;
+            for (int i = 1; i < pointLength; i++)
+            {
+                for(int j = i + 1; j < pointLength; j++)
+                {
+                    //두 점
+                    Point p0 = pointArray[i];
+                    Point p1 = pointArray[j];
+
+                    //방향
+                    int dir = CCW(pointArray[0],p0,p1);
+
+                    if (dir == 0)//일직선
+                    {
+                        //피벗점간의 거리 비교
+                        float dist0 = (pointArray[0].x - p0.x) * (pointArray[0].x - p0.x) + (pointArray[0].y - p0.y) * (pointArray[0].y - p0.y);
+                        float dist1 = (pointArray[0].x - p1.x) * (pointArray[0].x - p1.x) + (pointArray[0].y - p1.y) * (pointArray[0].y - p1.y);
+
+                        //스왑
+                        if (dist0 > dist1)
+                        {
+                            isSwap = true;
+                        }else isSwap = false;
+                    }
+                    else if (dir == -1)//시계
+                    {
+                        isSwap = true;
+                    }
+                    else {//반시계면 정상
+                        isSwap = false;
+                    }
+
+                    //Swap
+                    if (isSwap)
+                    {
+                        Point tempPoint = pointArray[i];
+                        pointArray[i] = pointArray[j];
+                        pointArray[j] = tempPoint;
+                    }
+                }
+            }
+            return pointArray.ToList();
         }
 
         /// <summary>
@@ -77,31 +130,31 @@ namespace ConVexHULL
         Stack<Point> RemovePointInLine(List<Point> pointList)
         {
             shapePointStack.Clear();
-            //첫 2개의점을 넣는다.
+
+            //첫 두개의 점 넣기
             shapePointStack.Push(pointList[0]);
             shapePointStack.Push(pointList[1]);
 
-            //점 순회하면서 시계방향일 경우 제거
             for (int i = 2; i < pointList.Count; i++)
             {
+                Point curPoint = pointList[i];
                 while(shapePointStack.Count >= 2)
                 {
-                    //위에서 2개의 점을 꺼낸다.
-                    Point p1 = shapePointStack.Pop();//맨위
-                    Point p2 = shapePointStack.Peek();//맨위에서 2번째
+                    //맨위 2개의 점
+                    Point p1 = shapePointStack.Pop();
+                    Point p2 = shapePointStack.Peek();
 
-                    //현재점까지 3개의 점으로 CCW를 사용해서 방향을 검사한다.
-                    Point curP = pointList[i];
-                    int dir = CCW(p2, p1, curP);
+                    //방향
+                    int dir = CCW(p2,p1,curPoint);
 
-                    //반시계 방향인가?
+                    //반시계
                     if (dir == 1)
                     {
-                        shapePointStack.Push(p1);//다시 넣는다.
+                        shapePointStack.Push(p1);
                         break;
                     }
                 }
-                shapePointStack.Push(pointList[i]);
+                shapePointStack.Push(curPoint);
             }
             return shapePointStack;
         }
